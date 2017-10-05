c569a69187201b5060d9f0505d68b8519a0935f4
xorz57 <fot@ceid.upatras.gr>
2017-10-05 03:38:10 +0300

    Added my "Hello, World!" script in C++

---
+++ scripts/hello_world.cpp
@@ -0,0 +1,10 @@
+// LANGUAGE: C++
+// AUTHOR: George Fotopoulos
+// GITHUB: https://github.com/xorz57
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
