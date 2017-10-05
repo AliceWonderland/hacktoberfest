e8a5f1ccad2195a386c43feb9883d20d4a1c8db3
Alice Chuang <bomberetta@gmail.com>
2017-10-05 05:10:33 -0400

    added in js script

--- scripts/hello_world_alicewonderland.js
+++ scripts/hello_world_alicewonderland.js
@@ -2,5 +2,41 @@
 // ENV: Node.js
 // AUTHOR: Alice Chuang
 // GITHUB: https://github.com/AliceWonderland
+// LINK: http://es6-features.org/
 
-console.log('Hello, World!');
+/*
+
+This hello world uses Javascript Objects and Classes. Creates a Person object using the Constructor function and a method greeting() that logs out and returns a greeting string.
+
+Written in ES5 and ES6 versions of Javascript. Comment out one version before running.
+
+*/
+
+// Javascript ES5
+var Person = function(name,location){
+  this.name = name;
+  this.place = location;
+}
+
+Person.prototype.greeting = function(name){
+  name = (name) ? name : this.name;
+  var str = "Hello, World! by " + name;
+  console.log(str);
+  return str;
+}
+
+// Javascript ES6
+class Person {
+  constructor (name='Anyonymous', location='USA') {
+    this.name = name;
+    this.place = location;
+  }
+  greeting (name=this.name) {
+    let str = "Hello, World! by " + name;
+    console.log(str);
+    return str;
+  }
+}
+
+var myself = new Person('Alice','NYC');
+myself.greeting();
