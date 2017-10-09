8fcafaf9bad703b26e1c305a16fcc166a7c0398d
nguyendhse <nguyendhse@gmail.com>
2017-10-09 09:01:18 +0700

    update author info

--- scripts/helloworld.dart
+++ scripts/helloworld.dart
@@ -1,6 +1,7 @@
-/**
-  * Hello world with Dart language
-  **/
+// LANGUAGE: Dart
+// ENV: DartVM
+// AUTHOR: Nguyen Do
+// GITHUB: https://github.com/nguyendhse
 void main() {
   print("Hello, World!");
 }
