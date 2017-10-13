61f9eb78c48df121e618b47829b9df4daa5c91cc
Elwin Martin <elwinmartin@users.noreply.github.com>
2017-10-13 13:55:00 -0700

    Create LISP Hello World
    Demonstrates Hello World functionality in Common LISP.

---
+++ scripts/hello_world.lisp
@@ -0,0 +1,10 @@
+; LANGUAGE: LISP
+; ENV: CLISP
+; AUTHOR: Elwin Martin
+; GITHUB: https://github.com/elwinmartin
+;
+; LISP is one of the oldest high-level languages
+; and dialects such as Common LISP and Clojure 
+; are still widely used today.
+
+(write-line "Hello, World!")
