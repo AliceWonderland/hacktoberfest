0758ac37126e78436f7f62bf34e0ee2796e084cc
TMTEAMZ <naiwheatleyth@outlook.com>
2017-10-13 20:24:10 +0700

    Create hello_world_tmteamz.js

---
+++ scripts/hello_world_tmteamz.js
@@ -0,0 +1,10 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Passawat Noraman
+// GITHUB: https://github.com/tmteamz
+
+(function() {
+  'use strict';
+  var ECHO = 'Hello World';
+    console.log(ECHO);
+})();
