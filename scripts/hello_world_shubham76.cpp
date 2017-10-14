0fdd5e268ba3db229ddbe91cfb44e440ca0c7636
Shubham R Patil <shubham.r.patil7@gmail.com>
2017-10-14 10:22:03 +0530

    Created Hello World file in C++

---
+++ scripts/hello_world_shubham76.cpp
@@ -0,0 +1,14 @@
+// LANGUAGE: C++
+// ENV: g++
+// AUTHOR: Shubham R Patil
+// GITHUB: https://github.com/shubham76
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  cout<<"Hello World!"<<endl;
+  
+  return 0;
+}
