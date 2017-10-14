e2731f6144094b64643250b7c1efd27dcbab457e
wildcardjoker <jason-github@jasonmooreit.com>
2017-10-14 11:19:11 +0800

    Add Hellow World PowerShell script.

---
+++ scripts/hello_world_wildcardjoker.ps1
@@ -0,0 +1,11 @@
+<#.Synopsis
+    Simple Hello World script
+.DESCRIPTION
+    Displays the text "Hello world!" to the console.
+.EXAMPLE
+    hellowworld_wildcardjoker
+
+    Display "Hello world" to console
+#>
+
+Write-Host "Hello world!"
