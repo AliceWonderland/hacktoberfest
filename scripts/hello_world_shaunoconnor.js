acf98e14cbd95a3cd5729c846f471b82ae5ff37d
Shaun O'Connor <shaunoconnors@hotmail.com>
2017-10-14 14:50:43 +0100

    Added hello world script

---
+++ scripts/hello_world_shaunoconnor.js
@@ -0,0 +1,10 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Shaun O'Connor
+// GITHUB: https://github.com/shaunoc09
+
+log('Hello, World!');
+
+function log(message) {
+    console.log(message);
+}
