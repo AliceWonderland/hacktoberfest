65595a510961fdf24dc8713f25120bd85fd7f4c1
cricketer94 <e0011768@u.nus.edu>
2017-10-15 17:37:15 +0800

    Add hello world script

---
+++ scripts/hello_worldx5.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+class HelloWorld {
+  public static void main (String args[]) {
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+  }
