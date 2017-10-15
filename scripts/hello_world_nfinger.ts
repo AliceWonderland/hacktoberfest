9d370e8b22c89a0f3492889b6b0904c8fcdb1ff8
Nathaniel Finger <nathanielfinger@Nathaniels-MacBook-Pro-2.local>
2017-10-15 10:49:34 -0400

    added bio and hello world

---
+++ scripts/hello_world_nfinger.ts
@@ -0,0 +1,7 @@
+// LANGUAGE: Typescript
+// ENV: Node.js
+// AUTHOR: Nathaniel Finger
+// GITHUB: https://github.com/nfinger
+
+// Long live programming and open source collaboration
+console.log('Hello, World!');
