f7681212ace2c000c15b24af636e37293a417118
Gus Gamble <lgamble20@gwu.edu>
2017-10-17 11:59:07 -0400

    creeated my hello world

---
+++ scripts/hello_world_gusgamble.java
@@ -0,0 +1,9 @@
+public class hello_world_gusgamble{
+	public static void main(String[] args){
+	
+	System.out.println("Hello World");
+	System.out.println("lowkey, does anyone even read the code submitted by anyone else?");
+
+
+	}
+}
