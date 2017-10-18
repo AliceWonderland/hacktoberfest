416cd63b29539563e58fe43b38e22532c39a21f4
Amandeep Singh <a.singh.2313@gmail.com>
2017-10-18 16:19:32 +0530

    Added my name in contributors.md and my hello world program in scripts folder

---
+++ scripts/HelloWorld.cpp
@@ -0,0 +1,12 @@
+//LANGUAGE: C++
+//ENV: HelloWorld.cpp
+//AUTHOR: Amandeep Singh
+//GITHUB: https://github.com/thisissingh
+
+#include<iostream>
+
+int main()
+{
+ std::cout<<"Hello World";
+ return 0;
+}
