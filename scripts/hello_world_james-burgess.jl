62db16c998c24657e4da40e204116f591e074fc6
james-burgess <jms.bgr@gmail.com>
2017-10-18 15:46:31 +0200

    created my own hello world in Julia (woop im first)

---
+++ scripts/hello_world_james-burgess.jl
@@ -0,0 +1,7 @@
+# LANGUAGE: Julia-Lang
+# ENV: Julia-Repl
+# AUTHOR: James Burgess
+# GITHUB: https://github.com/James-Burgess
+# EXTRA: link to my Month of Julia Project --> https://github.com/James-Burgess/MonthOfJulia
+
+println("hello world")
