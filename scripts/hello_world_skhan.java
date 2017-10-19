61fe5a789a6cdfde0e9e8b30bd8a171454b34548
Sameer Khan <sameer_khan@student.uml.edu>
2017-10-19 12:16:43 -0400

    Updated script

--- scripts/hello_world_skhan.java
+++ scripts/hello_world_skhan.java
@@ -1,3 +1,8 @@
+// LANGUAGE: Java
+// ENV: Eclipse
+// AUTHOR: Sameer Khan
+// GITHUB: https://github.com/sameerkhan116
+
 public class hacktoberfest {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
