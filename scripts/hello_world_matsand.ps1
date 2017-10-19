a6cb880c7bc6bc52e31cf727ac6e3ce4c4001ceb
Mats Andreassen <MatsAnd@users.noreply.github.com>
2017-10-19 11:54:48 +0200

    Added "Hello world"

---
+++ scripts/hello_world_matsand.ps1
@@ -0,0 +1,14 @@
+<#
+  .SYNOPSIS
+   Prints "Hello world" in the console
+
+  .DESCRIPTION
+    Is this necessary
+
+  .NOTES
+    Author:
+     - Mats Andreassen
+     - GitHub: https://github.com/MatsAnd
+#>
+
+Write-Host "Hello world"
