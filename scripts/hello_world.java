9a9329d1c0191268696fd3de2f24e42600c061c8
Aman Varma <amanvarma.aman5@gmail.com>
2017-10-19 13:00:27 +0530

    Added Exclamations

--- scripts/hello_world.java
+++ scripts/hello_world.java
@@ -7,7 +7,7 @@
 public class hello_world {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!!!!!");
     }
 
 }
