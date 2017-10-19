931ea4759c87af04fc6a6aaa2cded4a334e04f17
AnyST <daniurd@hotmail.com>
2017-10-19 00:10:10 -0200

    Hello, World! With C#
    First contribution to an open source project on GitHub.
    The program displays the message Hello World!
    This is my first Pull Request to celebrate HacktoberFest.

---
+++ hello_world_anyst.cs
@@ -0,0 +1,14 @@
+// LANGUAGE: C#
+// ENV: Visual Studio
+// AUTHOR: Daniele Patricia
+// GITHUB: https://github.com/AnyST
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+ }
