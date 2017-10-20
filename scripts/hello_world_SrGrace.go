4c79e426d3135111a577ca89bad12058ded723ac
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-10-20 20:39:35 +0530

    Updated hello_world_SrGrace.go

--- scripts/hello_world_SrGrace.go
+++ scripts/hello_world_SrGrace.go
@@ -1,8 +1,13 @@
+// LANGUAGE: GO
+// ENV: Go
+// AUTHOR: Sourav Verma
+// GITHUB: https://github.com/SrGrace
+
 package main
 
 import "fmt"
 
 func main() {
 	fmt.Printf("hello, world\n")
-  fmt.Printf("Lets go for a ride :)\n")
+  	fmt.Printf("Lets go for a ride :)\n")
 }
