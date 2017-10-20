47e17c36911129ad247681ad36645d02e222e20f
Indra Kusuma <indrakusuma.udinus@gmail.com>
2017-10-20 20:20:05 +0700

    update hello world from semarang

--- scripts/hello_world_ik.php
+++ scripts/hello_world_ik.php
@@ -1,6 +1,11 @@
-<!-- ================= -->
-<!-- Indra Kusuma -->
-<!-- Salam Kenal -->
-<!-- ================= -->
-
-<?php echo "Hello World!" ?>
+<?php
+  /*
+    -------------------------------------------
+    @LANGUAGE: PHP 
+    @ENV: Apache
+    @AUTHOR: Indra Kusuma
+    @GITHUB: https://github.com/idindrakusuma 
+    -------------------------------------------
+  */
+  echo "Hello World!" 
+?>
