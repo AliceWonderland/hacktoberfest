23450fccf09163be7bc9dac8889532f08534b749
Courtney Kirkham <therealckirkham@gmail.com>
2017-10-20 02:01:17 -0500

    add Hello World in c++
    I can't wait to see what everyone else did!

---
+++ scripts/hello_world_Puugu.cpp
@@ -0,0 +1,18 @@
+//This project is a hello world example for Hacktoberfest 2017
+/*Language: C++
+  IDE: Visual Studio 2017
+  Author: Puugu
+  Github: https://github.com/Puugu
+  Project Creation Date: 20 October 2017*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//output hello world message
+	cout << "Hello, world! I am Puugu. It's nice to meet you.\n";
+
+	//exit program
+	system("pause");
+	return 0;
+}
