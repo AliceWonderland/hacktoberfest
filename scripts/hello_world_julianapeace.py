f8b7f0636481909c249687e47734886c38f4bbf6
Julie <julianapeace@users.noreply.github.com>
2017-10-21 13:55:23 -0500

    Create hello_world_julianapeace.py

---
+++ scripts/hello_world_julianapeace.py
@@ -0,0 +1,12 @@
+# LANGUAGE: Python 3.6
+# AUTHOR: Juliana Mei
+# GITHUB: https://github.com/julianapeace
+
+def emojiLover():
+    emoji = ['🐶','🐱','🐧','🐝','🦄','🦋','☘️','🌺','⭐️','🔥','🌈','💦','🍕','🍭','😃','🚌','✈️','🔫','😂','💩','👻','🙌','💙','💚','💛']
+    print ("Hello World, my name is Julie and I LOVE Emojis!!")
+    print("These are some of my favorite emojis:")
+    for i in emoji:
+        print(i)
+
+emojiLover()
