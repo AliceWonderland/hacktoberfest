0881a50bbb1e55cd19f8fd8881b0769b1285d4d7
Tuhin Khare <tuhinkhare3@gmail.com>
2017-10-22 21:01:06 +0530

    First Contribution to HacktoberFest

---
+++ scripts/hello_world_tuhinkhare.cpp
@@ -0,0 +1,12 @@
+// LANGUAGE: C++
+// ENV: Clang
+// AUTHOR: Tuhin Khare
+// GITHUB: https://github.com/whiz-Tuhin
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cout<<"Hello World"<<endl;
+}
