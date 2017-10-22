1b5c05d027555fb45d26f00fa981a33436dce4c5
ramanandY <ramanand359@gmail.com>
2017-10-22 11:42:11 +0530

    Hello world in java

---
+++ scripts/ramanandY.java
@@ -0,0 +1,13 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Ramanand Yadav
+  GITHUB: https://github.com/ramanandY
+*/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
