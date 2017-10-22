4e82c2e5418bcedb5b678c97f237e26362c5d61a
TheGameFreak720 <dj-entityofthedark@live.com>
2017-10-21 23:14:07 -0400

    Added hello script

---
+++ scripts/hello_world_luis_alvarez.js
@@ -0,0 +1,10 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Luis M. Alvarez Pacheco
+// GITHUB: https://github.com/TheGameFreak720
+
+var helloWorld = "Hello World";
+
+console.log(helloWorld);
+alert(helloWorld);
+return helloWorld;
