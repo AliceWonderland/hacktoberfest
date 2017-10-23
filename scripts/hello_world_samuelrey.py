209ed6fba03c156125f101a0e1990131bcdb1281
Samuel Villavicencio <samuelrey010@gmail.com>
2017-10-23 13:48:31 -0700

    Add helloworld script

---
+++ scripts/hello_world_samuelrey.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# LANGUAGE: python 
+# ENV: python 
+# AUTHOR: Samuel Villavicencio
+# GITHUB: https://github.com/samuelrey
+def helloworld():
+    print 'hello world'
+
+if __name__ == '__main__':
+    helloworld()
