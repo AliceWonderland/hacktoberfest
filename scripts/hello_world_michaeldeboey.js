4d74d3b6d0a7f399e23d1d145de3c279c76db390
Michaël De Boey <info@michaeldeboey.be>
2017-10-23 12:13:24 +0200

    Add Michaël De Boey to scripts

---
+++ scripts/hello_world_michaeldeboey.js
@@ -0,0 +1,39 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Michaël De Boey
+// GITHUB: https://github.com/MichaelDeBoey
+// LINK: http://es6-features.org/
+
+// This hello world uses Javascript Objects and Classes.
+// Creates a Person object using a Constructor function and a method greeting() that logs out and returns a greeting string.
+// Written in ES5 and ES6 versions of Javascript. Comment out one of the versions before running.
+
+
+// Javascript ES5
+var Person = function(name,location){
+    this.name = (name) ? name : "Anyonymous";
+    this.place = (location) ? location : "USA";
+};
+
+Person.prototype.greeting = function(name){
+    name = (name) ? name : this.name;
+    var str = "Hello, World! by " + name;
+    console.log(str);
+    return str;
+};
+
+// Javascript ES6
+class Person {
+    constructor (name='Anyonymous', location='USA') {
+        this.name = name;
+        this.place = location;
+    }
+    greeting (name=this.name) {
+        let str = "Hello, World! by " + name;
+        console.log(str);
+        return str;
+    }
+}
+
+var myself = new Person('Michaël De Boey','Sint-Niklaas, Belgium');
+myself.greeting();
