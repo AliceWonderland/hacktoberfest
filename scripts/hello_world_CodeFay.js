33cd3e8fb6ad9138994dd2bde91a68740dfb2918
Yang <204067485@logon.ds.ge.com>
2017-10-23 15:27:04 -0500

    CodeFay HACKTOBERFEST

---
+++ scripts/hello_world_CodeFay.js
@@ -0,0 +1,8 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: CodeFay
+// GITHUB: https://github.com/CodeFay
+
+console.log('Hello, World!');
+
+// I chose javascript because it's the first web dev language that I have learned.  I've previously dabbled in MATLAB and Fortran, and now I am also dabbling in C for the edX CS50 course!
