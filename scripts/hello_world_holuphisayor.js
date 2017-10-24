11be6097ca058cab826d48ee5d52a118c3e6ceae
olufisayo <juliematheson95@gmail.com>
2017-10-24 18:46:59 +0000

    Added hello_world_holuphisayor.js

---
+++ scripts/hello_world_holuphisayor.js
@@ -0,0 +1,11 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Olufisayo Akinmoju
+// GITHUB: https://github.com/holuphisayor
+
+
+function sayHello(word){
+  console.log('Hello ' + word);
+};
+
+sayHello('world')
