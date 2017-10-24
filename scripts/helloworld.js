faae8fd64d35201f4e31f765228549a61d700893
Siffrein <siffr.hdesigy@gmail.com>
2017-10-24 23:27:00 +0200

    Update helloworld.js

--- scripts/helloworld.js
+++ scripts/helloworld.js
@@ -1,1 +1,6 @@
-console.log("Hello world !")
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Siffrein Sigy
+// GITHUB: https://github.com/siffreinsg
+
+console.log("Hello World !! So happy to participate to the HacktoberFest ! LOVE YOU GUYS <3")
