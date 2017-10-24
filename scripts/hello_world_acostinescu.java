0fa65f87e93eac2acd6c3a87be8f488a32bfec15
Alex Costinescu <alex@TouchbarNinety.local>
2017-10-24 13:18:01 -0400

    Completed all 3 tasks!

---
+++ scripts/hello_world_acostinescu.java
@@ -0,0 +1,12 @@
+// LANGUAGE: JAVA
+// ENV: Atom
+// AUTHOR: Alex Costinescu
+// GITHUB: https://github.com/acostinescu
+
+public class hello_world_acostinescu
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+  }
+}
