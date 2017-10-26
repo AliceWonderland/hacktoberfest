fd4b37e7d8b7acf0d27bc6d627cf2b84b231ea0c
elmuri <elmuri@users.noreply.github.com>
2017-10-26 21:38:25 +0300

    just doing some contributions
    Added own helloworld

---
+++ scripts/Hello_World_Elmuri.py
@@ -0,0 +1,13 @@
+Hello = ["H","e","l","l","o"]
+World = ["W","o","r","l","d"]
+
+thing = ""
+other_thing = ""
+
+for i in Hello:
+    thing = thing + i
+    
+for i in World:
+    other_thing = other_thing + i
+    
+print (thing + other_thing)
