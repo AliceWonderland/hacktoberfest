6e7686331b156ee85b9df69af8199a170fbe9d63
Narayana Suri <narayanasuri08@gmail.com>
2017-10-26 07:52:44 -0700

    add-suri branch initial

--- scripts/hello_world_ns.java
+++ scripts/hello_world_ns.java
@@ -3,7 +3,6 @@
 // AUTHOR: Narayana Suri
 // GITHUB: https://github.com/narayanasuri
 
-
 public class hello_world_ns {
  public static void main(String[] args){
   System.out.println("Hello World!");
