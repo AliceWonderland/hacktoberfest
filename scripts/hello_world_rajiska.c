fefc333e6c8450ff393a18dc5507f945866f6096
RaJiska <dodo.lasticot@gmail.com>
2017-10-26 19:12:37 +0200

    Hello World !

---
+++ scripts/hello_world_rajiska.c
@@ -0,0 +1,15 @@
+/*
+ *  File	: hello_world_rajiska.c
+ *  Author	: Doriann "Ra'Jiska" Corlouër
+ *  Created	: Thu Oct 26 19:11:45 2017
+ *  Updated on	: Thu Oct 26 19:12:24 2017
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  puts("Hello World");
+  return EXIT_SUCCESS;
+}
