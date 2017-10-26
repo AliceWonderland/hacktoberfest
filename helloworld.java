7e51e064a46bce2d01f1139c1fd38ed9e1c86a1e
varadkulkarni11 <varadkulkarnirules@gmail.com>
2017-10-27 01:02:03 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      
+      System.out.println("Hello, World");
+   }
+}
