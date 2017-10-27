dc0b5ae766a8d50444f24f9522625a9782ee9dec
Julius Breckel <julius.breckel@gmail.com>
2017-10-27 11:46:00 +0200

    create duplicate checker for CONTRIBUTERS.md

---
+++ check-contributors-duplicates/index.js
@@ -0,0 +1,33 @@
+const { isEmpty } = require('lodash')
+const { readFile, writeFile } = require('fs-extra')
+
+readFile('../CONTRIBUTORS.md', 'utf-8').then((contributers) => {
+  const uniq = []
+  contributers.split('\n\n').map(contributer =>
+    contributer.split('\n').map(
+      line =>
+        line.replace(/^\s+/, '').replace('- ', '').replace(/^#+ /, '').split(': ')
+    ).reduce(
+      (prev, line) => {
+        if (!line[1]) return prev
+        return Object.assign({}, prev, { [line[0]]: line[1]})
+      }, {}
+    )
+  )
+    .filter(e => !isEmpty(e))
+    .reduce(
+      (prev, contrib) => {
+        if(contrib && prev[JSON.stringify(contrib)]) {
+          return prev
+        }
+        uniq.push(contrib)
+        return Object.assign({}, prev, { [JSON.stringify(contrib)]: true })
+      }, {}
+    )
+  const newFile = uniq.map(({ Name, ...rest }) => {
+    return `#### Name: ${Name}
+${Object.keys(rest).map(key => `- ${key}: ${rest[key]}`).join('\n')}
+`
+  }).join('\n')
+  writeFile('../CONTRIBUTORS.md', newFile, 'utf-8').catch(reason => console.log(reason))
+})
