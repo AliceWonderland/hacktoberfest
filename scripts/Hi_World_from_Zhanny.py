d23d6cdd013b3973280f12c28564c5a328b6f0c0
Zhanet <ivanova.zhanet@gmail.com>
2017-10-28 20:49:22 +0100

    Added Zhanny's greeting to World

---
+++ scripts/Hi_World_from_Zhanny.py
@@ -0,0 +1,9 @@
+# LANGUAGE: Python
+# ENV: Python3.6
+# AUTHOR: Zhanet Ivanova
+# GITHUB: https://github.com/mushmula27
+
+def hi_peeps():
+    print("Hello Wold, I hope you are well. Let us all love kittehs.")
+
+hi_peeps()
