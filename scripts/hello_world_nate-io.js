09855c9f2af329181d5a5a744461e1cf2993a8d7
nate young <nyoung125@gmail.com>
2017-10-28 12:08:01 -0400

    add hacktoberfest changes

---
+++ scripts/hello_world_nate-io.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AWESOME NODE COURSE: https://learnnode.com/
+// AUTHOR: Nate Young
+// GITHUB: https://github.com/nate-io
+
+console.log(`What's Crackin' World!`);
