65a54c162ae969f37829732e92c443ef20dd2d20
Aminu Ibrahim Abubakar <aminukano585@gmail.com>
2017-10-29 00:43:34 +0100

    Server side hello world
    A simple server side hello world script

--- scripts/hello_world_AminuSufi585.js
+++ scripts/hello_world_AminuSufi585.js
@@ -3,4 +3,13 @@
 // AUTHOR: Aminu Ibrahim Abubakar
 // GITHUB: https://github.com/AminuSufi585
 
-console.log('Hello, World!');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Server started....');
+});
