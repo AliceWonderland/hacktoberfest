f7e8f4c94e80a6be43688bf951e9b3a098d03bb8
rs-rohan <32260864+rs-rohan@users.noreply.github.com>
2017-10-28 17:43:20 +0000

    C++ and C combo Hello World
    Combo Hello World

---
+++ hello_world_incpp.cpp
@@ -0,0 +1,13 @@
+// Language 	C++
+// Author 	Rohan Sharma
+// GitHub	https://github.com/rs-rohan
+
+
+
+#include<stdio.h>
+#include<iostream>
+int main()
+{
+	printf("Hello");
+	puts(" World!");
+}
