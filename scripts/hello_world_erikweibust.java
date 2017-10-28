493187b096fa8f9d964c392d50401870bf67f547
Erik Weibust <erik@weibust.net>
2017-10-28 17:41:10 -0500

    added java script to project

---
+++ scripts/hello_world_erikweibust.java
@@ -0,0 +1,11 @@
+// LANGUAGE: Java
+// ENV: JVM
+// AUTHOR: Erik Weibust
+// GITHUB: https://github.com/erikweibust
+
+public class hello_world_erikweibust {
+
+	public static void main( String[] args ) {
+		System.out.println( "Hello, World! );
+	}
+}
