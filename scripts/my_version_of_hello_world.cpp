ffd9fe9a0dfed0974600fe96779a295cf5380680
Mohit-Nathrani <mohitkn01@gmail.com>
2017-10-30 03:31:56 +0530

    my-version-of-hello-world

---
+++ scripts/my_version_of_hello_world.cpp
@@ -0,0 +1,9 @@
+//my_version_of_hello_world
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[]={72,101,108,108,111,44,32,87,111,114,108,100,33};
+	for(int i=0;i<13;i++){ cout<<(char)a[i]; }
+	return 0;
+}
