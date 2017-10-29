1a6ff7ea36e7a5ddd02d6910d0a6da9527beeedc
Tharindu Dilshan <tdilshan2010@gmail.com>
2017-10-29 13:20:14 +0530

    I created a sample Hello World program

---
+++ scripts/Tharindu.java
@@ -0,0 +1,13 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Tharindu Dilshan Sooriyaarachchi
+  GITHUB: https://github.com/TharinduDilshan
+*/
+
+public class Tharindu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
