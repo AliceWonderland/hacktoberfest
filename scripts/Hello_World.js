8713bcc484024c6e7bd8e2ade3bb840ded38079b
swatikanwal <kanwalswati97@gmail.com>
2017-10-29 13:07:50 +0530

    pull_req

---
+++ scripts/Hello_World.js
@@ -0,0 +1,6 @@
+// LANGUAGE: JavaScript
+// ENV: Sublime Text 
+// AUTHOR: Swati Kanwal 
+// GITHUB: https://github.com/swatikanwal
+
+console.log("Hello WOrld :) I'm Swati and I'm currently a sophomore at college");
