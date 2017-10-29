cfeb2db80d9a3338685e8e6994f5190eee3a187b
ekam walia <ekamwalia@gmail.com>
2017-10-29 21:22:33 +0530

    Modified code according to guidelines

---
+++ scripts/hello_world_ekamwalia.go
@@ -0,0 +1,15 @@
+//Language: Go
+//Author: Ekam Walia
+//GitHub: https://github.com/EkamWalia
+package main
+
+import "fmt"
+
+func main() {
+
+	// I recently started working with Golang 
+	// And incorporating my new found interest 
+	// I chose to write the hello world program in Golang
+
+	fmt.Println("Hello, World")
+}
