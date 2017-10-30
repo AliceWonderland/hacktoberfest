40cc6ee9e3f2209d9b0c04c523e1ebdecd7723a7
saumyabahu <saumyabahu@gmail.com>
2017-10-31 01:01:15 +0530

    Update HelloWorldPadrones.java

--- HelloWorldPadrones.java
+++ HelloWorldPadrones.java
@@ -8,8 +8,25 @@
 public class HelloWorldPadrones {
 
 	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("enter size of array");
+		int n=in.nextInt();
+		int pro=1;
+		int p1=1;
+		for(int i=0;i<n;i++)
+		{
+			int p1=p1*a[i];
+		}
 		
-		System.out.println("Hello World");
+		for(int i=1;i<n;i++)
+		{
+			pro=pro*i;
+		}
+		int div=pro/p1;
+		System.out.println("missing number is",div);
+		
+		
+		
 
 	}
 
