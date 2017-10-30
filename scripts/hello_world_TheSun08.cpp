87cbfebb40f72bfde460bcad53d60db5515f437e
TheSun08 <the.sun.the.only.one@gmail.com>
2017-10-30 23:41:17 +0300

    Create hello_world_TheSun08.cpp

---
+++ scripts/hello_world_TheSun08.cpp
@@ -0,0 +1,10 @@
+// LANGUAGE: C++
+// ENV: CLion
+// AUTHOR: Ekaterina Sterzhanova
+// GITHUB: https://github.com/TheSun08
+#include <iostream>
+
+int main() {
+  std::cout « "Hello, World!";
+  return 0;
+}
