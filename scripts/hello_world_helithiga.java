73d801e0b04fd13b3f1f92e8df89e6dc819dd07b
helithiga <33217879+helithiga@users.noreply.github.com>
2017-10-30 15:53:26 +0530

    Create hello world

---
+++ scripts/hello_world_helithiga.java
@@ -0,0 +1,12 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Alice Chuang
+// GITHUB: https://github.com/AliceWonderland
+
+class hello_world_helithiga{
+ public static void main(Stirng[]args){
+  System.out.println("Hello World")
+ }
+ 
+
+}
