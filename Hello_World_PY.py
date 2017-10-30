c0dcc31a4417325e43a6d016fd5bc45559dd1cfc
Palak Bansal <palakb1406@gmail.com>
2017-10-31 01:20:52 +0530

    Update Hello_World_PY.py
    Bank Account

--- Hello_World_PY.py
+++ Hello_World_PY.py
@@ -2,3 +2,25 @@
 # AUTHOR: Nikhil
 # GITHUB: https://github.com/computelarge
 print("Hello World!")
+print("Hacktoberfest");
+class BankAccount:
+    
+    balance = 0
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+a = BankAccount()
+b = BankAccount()
+x = a.deposit(100)
+print("Balance in a after deposit is : " ,x)
+y = b.deposit(50)
+print("Balance in b after deposit is : " ,y)
+p = b.withdraw(10)
+print("Balance in b after withdraw is : " ,p)
+q = a.withdraw(10)
+print("Balance in a after withdraw is : " ,q)
