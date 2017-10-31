eeede8bcc14b6213c7536a338dfed881d0c1bb36
Hüseyin Hasan <hsynchsn@gmail.com>
2017-10-31 19:25:14 +0300

    Add files via upload

---
+++ scripts/hello_world_hsynchsn.cs
@@ -0,0 +1,15 @@
+﻿// LANGUAGE: C#
+// ENV: Visual Studio 2017
+// AUTHOR: Huseyin Hasan
+// GITHUB: https://github.com/hsynchsn
+
+namespace hello_world_hsynchsn  
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
