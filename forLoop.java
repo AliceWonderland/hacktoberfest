456b5824768f3c1e3cea8be1ea7779ba09821e50
wdevon99v2 <wdevon99@outlook.com>
2017-10-31 10:57:19 +0530

    make new file called forloop.java

---
+++ forLoop.java
@@ -0,0 +1,22 @@
+/*
+ * LANGUAGE: JAVA
+ * AUTHOR: Gelle Padrones
+ * GITHUB: https://github.com/aegelle31
+ * 
+ * **/
+// this is first program everyone should start from..
+public class HelloWorldPadrones {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<20;i++){
+			System.out.println( i+ " . Hello World");
+
+		}
+
+		System.out.println("Hello World");
+                 
+
+	}
+
+}
