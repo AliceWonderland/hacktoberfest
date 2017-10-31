00ec20b63a189a90e5f05271197002221c1aa188
Mik <nery.mik@gmail.com>
2017-10-31 13:57:42 -0200

    whoops, didnt see the rules before

--- helloooo.c
+++ helloooo.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
+ /* Name: Miguel (https://github.com/MiguelNery)
+  - Place: Brasilia, Distrito Federal, Brasil
+  - Bio: I am Miguel (:
+  - GitHub: MiguelNery (https://github.com/MiguelNery)
+*/
+
 int main() {
 
 printf("bye");
