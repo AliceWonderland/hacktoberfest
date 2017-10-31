08f51b90131a366c609d7024b12310b16474466f
CameronFc <m35191970@gmail.com>
2017-10-31 18:19:39 -0300

    Added another Swift HelloWorld.

---
+++ scripts/HelloWorld_CameronFc.swift
@@ -0,0 +1,7 @@
+// HelloWorld in Swift - With the usual iOS application entry point inside
+// AppDelegate.
+
+func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+  print("Hello world!")
+  return true   
+}
