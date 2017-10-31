f92b67691db1c768fb20724497d78b173e3ab9ef
Joshsoshm <33269642+Joshsoshm@users.noreply.github.com>
2017-10-31 20:51:15 -0300

    Create helllooworld.cpp

---
+++ scripts/helllooworld.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<string.h>
+
+int main()
+{
+  char name[20];
+  cout<<"Enter name ";
+  gets(name);
+  cout<<" Hello " << name <<"\nYou mean the world to someone";
+  return 0;
+}
