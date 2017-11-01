4b4bc7bbc2f0db1ea8c927e02d3facbafc43dff1
Pedro Castilho <cstl.px@gmail.com>
2017-10-31 21:05:35 -0300

    Added Hello World in Common Lisp

---
+++ scripts/hello_world.lisp
@@ -0,0 +1,4 @@
+;;language: Common Lisp
+;;format works like printf in C - it formats a string and prints it
+;;using "t" as the first argument of format sends output to stdout
+(format t "Hello, World!")
