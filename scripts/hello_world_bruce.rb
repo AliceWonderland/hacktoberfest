8aca914077d844219dcc453855b9ddedb13e8510
brucewillke <brucewillke@users.noreply.github.com>
2017-11-01 13:04:25 -0700

    updating data and formatting to fit requirements.

--- scripts/hello_world_bruce.rb
+++ scripts/hello_world_bruce.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/ruby
 
+# LANGUAGE: ruby
+# AUTHOR: Bruce Willke
+# GITHUB: https://github.com/brucewillke
+
 def saythis (arg)
-	puts "#{arg}"
+    puts "#{arg}"
 end
 
 saythis("hello world")
