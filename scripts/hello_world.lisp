17e55c22f8d1e9a57fc02261142102b60ef0d686
Pedro Castilho <cstl.px@gmail.com>
2017-10-31 21:09:25 -0300

    Standardizing file

--- scripts/hello_world.lisp
+++ scripts/hello_world.lisp
@@ -1,4 +1,9 @@
-;;language: Common Lisp
+;;LANGUAGE: Common Lisp
+;;ENV: Any standards-compliant Common Lisp environment
+;;     such as CMUCL (https://www.cons.org/cmucl/)
+;;AUTHOR: Pedro Castilho
+;;GITHUB: https://github.com/pcstl
+
 ;;format works like printf in C - it formats a string and prints it
 ;;using "t" as the first argument of format sends output to stdout
 (format t "Hello, World!")
