0636ab494a2746f70fad25ccb0499531d7bb0a5b
Aetheryx <zain1@live.nl>
2017-11-01 11:44:19 +0100

    Update hello_world_aetheryx.js

--- scripts/hello_world_aetheryx.js
+++ scripts/hello_world_aetheryx.js
@@ -1,8 +1,9 @@
-const System = { out: { println: console.log } };
-
-System.out.println('Hello World!'); // wait, what?
-
 // LANGUAGE: JavaScript
 // ENV: Node.js
 // AUTHOR: Zain Ali
 // GITHUB: https://github.com/Aetheryx
+
+const System = { out: { println: console.log } };
+
+System.out.println('Hello World!'); // wait, what?
+
