d3a86539ba2683e341a01713b7e75bdf05b59eef
Aetheryx <zain1@live.nl>
2017-11-01 11:38:27 +0100

    Add metadata

--- scripts/hello_world_aetheryx.js
+++ scripts/hello_world_aetheryx.js
@@ -1,3 +1,8 @@
 const System = { out: { println: console.log } };
 
 System.out.println('Hello World!'); // wait, what?
+
+// LANGUAGE: JavaScript
+// ENV: Node.js
+// AUTHOR: Zain Ali
+// GITHUB: https://github.com/Aetheryx
