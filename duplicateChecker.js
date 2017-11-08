893d859db85de11935f0efef74aaafb5f8c390aa
Shashank <shashank.keshava@bookmyshow.com>
2017-11-08 18:48:17 +0530

    Feature invalidContext and duplicate checker

--- duplicateChecker.js
+++ duplicateChecker.js
@@ -7,16 +7,42 @@ const regEx = {
     Place: /(?=Place:).*/g,
     Bio: /(?=Bio:).*/g,
     GitHub: /(?=GitHub:).*/g
-}
+};
 
 let filteredObj = {};
+let uniqueObjNames = [];
+
+function uniqueValidator(name, next) {
+    if (uniqueObjNames.indexOf(name)) next({ duplicate: true });
+    else {
+        uniqueObjNames.push(name);
+        next(false);
+    }
+}
 
 function contributorExtractor(contributor, type, next) {
-    let temp = !!(contributor.match(regEx[type])[0]) ? (contributor.match(regEx[type])[0]) : false;
-    if (temp) {
-        temp = temp.replace(`${type}:`, "");
-        next(false, temp);
-    } else next(true);
+    let temp = null;
+
+    if (contributor.match(regEx[type]) == null)
+        next({ invalidContext: true }, contributor);
+    else {
+        if (contributor.match(regEx[type]) == null) debugger;
+
+        temp = !!contributor.match(regEx[type])[0]
+            ? contributor.match(regEx[type])[0]
+            : false;
+
+        if (temp) {
+            temp = temp.replace(`${type}:`, "");
+            if (type == "Name") {
+                uniqueValidator(temp, (err, res) => {
+                    if (err) {
+                        next(err,contributor);
+                    } else next(false, { [type]: temp });
+                });
+            } else next(false, { [type]: temp });
+        } else next({ invalidContext: true }, contributor);
+    }
 }
 
 fs.readFile("CONTRIBUTORS.md", "utf-8", (err, contributors) => {
@@ -24,20 +50,21 @@ fs.readFile("CONTRIBUTORS.md", "utf-8", (err, contributors) => {
 
     let filteredList = contributors.match(regEx.global);
     filteredList.map((contributor, index) => {
-        ["Name", "Place", "Bio", "GitHub"].every(type => {
-            let skip = false;
+        filteredObj[index] = {};
+        ["Name", "Place", "Bio", "GitHub"].forEach(type => {
+            //let skip = false;
             contributorExtractor(contributor, type, (err, res) => {
                 if (err) {
-                    skip = true;
-                    !!filteredObj[index] ? delete filteredObj[index] : false;
+                    Object.assign(filteredObj[index], err,...res);
+                    //skip = true;
+                    //!!filteredObj[index] ? delete filteredObj[index] : false;
                 } else {
-                    filteredObj[index] = {};
-                    filteredObj[index][type] = "";
-                    filteredObj[index][type] = res;
+                    Object.assign(filteredObj[index], res);
                 }
             });
-            if (skip)
-                return false;
-        })
-    })
-})
+            //if (skip) return false;
+        });
+        console.log(filteredObj[index]);
+        //console.log(Object.keys(filteredObj[index]).length);
+    });
+});
