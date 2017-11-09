cc98120ea6577d0b79d5410c64e62cbccd378755
Shashank <shashank.keshava@bookmyshow.com>
2017-11-09 13:21:18 +0530

    Completed duplicate checker and formatter

--- duplicateChecker.js
+++ duplicateChecker.js
@@ -1,59 +1,68 @@
-const fs = require("fs");
+/**
+ * Author: Shashank K
+ * Env: NodeJS
+ * Language: Javascript
+ * GitHub: https://github.com/shashankKeshava
+ **/
 
-// Matches contributors who have follwed the conventions in README.md
+/**
+ * Matches the contributors who have followed the conventions in README.md
+ * Convention:
+ * #### Name: [YOUR NAME](GitHub link)
+ * - Place: City, State, Country
+ * - Bio: Who are you?
+ * - GitHub: [GitHub account name](GitHub link)
+ **/
+
+const fs = require("fs");
 const regEx = {
     global: /#{4}\s(.*\n){4}/g,
     Name: /(?=Name:).*/g,
     Place: /(?=Place:).*/g,
     Bio: /(?=Bio:).*/g,
-    GitHub: /(?=GitHub:).*/g //To include Git(h/H)ub replace regEx expression with "/(?=Git[H|h]ub:*/g)" 
+    GitHub: /(?=Git[H|h]ub:).*/g //To include Git(h/H)ub replace regEx expression with "/(?=Git[H|h]ub:*/g)"
 };
 
 let filteredObj = {};
 let uniqueObjNames = [];
+let contributorMod = "";
 let count = {
     duplicate: 0,
-    invalidContext: 0,
     total: 0,
-    passed:0
+    passed: 0
 };
-let contributorMod=null;
 
 function uniqueValidator(name, next) {
-    let validation =
-        uniqueObjNames.indexOf(name) > -1
-            ? { duplicate: true }
-            : uniqueObjNames.push(name);
+    let validation = uniqueObjNames.indexOf(name) > -1 ? { duplicate: true } : uniqueObjNames.push(name);
     next(validation);
-
-    /* if (uniqueObjNames.indexOf(name)>-1) next({ duplicate: true });
-    else {
-        uniqueObjNames.push(name);
-        next(false);
-    }*/
 }
 
 function contributorExtractor(contributor, type, next) {
     let temp = null;
-
     if (contributor.match(regEx[type]) == null)
-        next({ invalidContext: true }, { error: { [type]: contributor } });
+        next({ invalidContext: true }, {
+            error: {
+                [type]: contributor
+            }
+        });
     else {
-        if (contributor.match(regEx[type]) == null) debugger;
-
-        temp = !!contributor.match(regEx[type])[0]
-            ? contributor.match(regEx[type])[0]
-            : false;
+        temp = !!contributor.match(regEx[type])[0] ? contributor.match(regEx[type])[0] : false;
 
         if (temp) {
-            temp = temp.replace(`${type}:`, "");
+            temp = (type == "GitHub" && contributor.match(/(?=Github).*/g)) ? temp.replace("Github", "") : temp.replace(`${type}:`, "");
             if (type == "Name") {
                 uniqueValidator(temp, (err, res) => {
                     if (err) {
-                        next(err, { [type]: temp });
-                    } else next(false, { [type]: temp });
+                        next(err, {
+                            [type]: temp
+                        });
+                    } else next(false, {
+                        [type]: temp
+                    });
                 });
-            } else next(false, { [type]: temp });
+            } else next(false, {
+                [type]: temp
+            });
         } else next({ invalidContext: true }, contributor);
     }
 }
@@ -61,58 +70,47 @@ function contributorExtractor(contributor, type, next) {
 function contributorsFormatter(next) {
     fs.readFile("CONTRIBUTORS.md", "utf-8", (err, contributors) => {
         if (err) console.log("Error:", err);
-
         let filteredList = contributors.match(regEx.global);
         filteredList.map((contributor, index) => {
             filteredObj[index] = {};
             ["Name", "Place", "Bio", "GitHub"].forEach(type => {
-                //let skip = false;
                 contributorExtractor(contributor, type, (err, res) => {
-                    if (err) {
-                        Object.assign(filteredObj[index], err, res);
-                        //skip = true;
-                        //!!filteredObj[index] ? delete filteredObj[index] : false;
-                    } else {
-                        Object.assign(filteredObj[index], res);
-                    }
+                    err ? Object.assign(filteredObj[index], err, res) : Object.assign(filteredObj[index], res);
                 });
-                //if (skip) return false;
             });
             console.log(filteredObj[index]);
-            //console.log(Object.keys(filteredObj[index]).length);
         });
         next(false, filteredObj);
     });
 }
-/*    count.total = Object.keys(filteredObj).length;
-    Object.keys(filteredObj).map(contributor => {
-        if (contributor.invalidContext) count.invalidContext++;
-        if (contributor.duplicate) count.duplicate++;
-    });
-    console.log(
-        `Total:${count.total} Duplicates:${count.duplicate} Invalid:${count.invalidContext}`
-    );
-});*/
 
-
-function contributorCleanUp(next){
+function contributorCleanUp(next) {
     count.total = Object.keys(filteredObj).length;
     Object.keys(filteredObj).map(index => {
-        //if (!!filteredObj[index]&&!!filteredObj[index]["invalidContext"]) {count.invalidContext++; delete filteredObj[index];}
-        if (!!filteredObj[index]&&!!filteredObj[index]["duplicate"]) count.duplicate++; 
-        if(!!filteredObj[index]&&!filteredObj[index]["invalidContext"]&&!filteredObj[index]["duplicate"]) {count.passed++;{
+        if (!!filteredObj[index] && !!filteredObj[index]["duplicate"])
+            count.duplicate++;
+        if (!filteredObj[index]["invalidContext"] && !filteredObj[index]["duplicate"]) {
             count.passed++;
-        contributorMod=contributorMod+`#### Name:${filteredObj[index]["Name"]}\n- Place:${filteredObj[index]["Place"]}\n- Bio:${filteredObj[index]["Bio"]}\n- GitHub:${filteredObj[index]["GitHub"]}\n`;}
+            contributorMod =
+                contributorMod +
+                `#### Name:${filteredObj[index]["Name"]}\n- Place:${
+                    filteredObj[index]["Place"]
+                }\n- Bio:${filteredObj[index]["Bio"]}\n- GitHub:${
+                    filteredObj[index]["GitHub"]
+                }\n\n`;
         }
     });
-    next(false,contributorMod);
+    next(false, contributorMod);
 }
 contributorsFormatter((err, res) => {
-    contributorCleanUp((err,res)=>{
-        fs.writeFile('CONTRIBUTORS_MOD.md',res,(err)=>{
-            console.log("Write Done");
-        })
+    contributorCleanUp((err, res) => {
+        fs.writeFile("CONTRIBUTORS_MOD.md", res, err => {
+            console.log("\n Write Completed: CONTRIBUTORS_MOD.md\n");
+        });
         console.log(
-            `Total:${count.total} Duplicates:${count.duplicate} Invalid:${count.invalidContext} Passed:${count.passed}`
-        );})
+            `\n\nTotal:${count.total} Duplicates:${count.duplicate} Passed:${
+                count.passed
+            } \n\n ***** The Count does not include Contributors using invalid convenction *****`
+        );
+    });
 });
