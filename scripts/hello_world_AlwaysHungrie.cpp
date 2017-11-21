dae04ff05f1b4c0580e67404f794a0f73be9b669
AlwaysHungrie <dhairyashah98@gmail.com>
2017-11-21 19:34:23 +0530

    AlwaysHungrie first commit hacktober

---
+++ scripts/hello_world_AlwaysHungrie.cpp
@@ -0,0 +1,12 @@
+// Language : C++
+// Compiler : g++ 
+// Author : Dhairya
+// GITHUB : https://github.com/AlwaysHungrie
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello World"<<endl;
+	return 0;
+}
