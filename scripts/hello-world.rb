d63b319e001c776282f8514066a96eed03e9b8de
Raghuram42 <voruganti.raghuram@gmail.com>
2017-12-06 18:13:32 +0530

    Open commit-2

--- scripts/hello-world.rb
+++
@@ -1,2 +0,0 @@
-puts 'Hello World!!'
-puts 'I\'m coming to fix the things up get ready my dear World'
---
+++ scripts/hello_world_raghuram42.rb
@@ -0,0 +1,6 @@
+# Language: Ruby
+# Author: Raghuram Voruagnti
+# GitHub: https://github.com/raghuram42
+
+puts 'Hello World!!'
+puts 'I\'m coming to fix the things up get ready my dear World'
