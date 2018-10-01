168c80a7de0643789d8b7ba69f82e207e54a81d5
Prateek Gupta <prateekgupta@Prateeks-MacBook-Air.local>
2018-10-01 07:37:08 +0530

    Updated scripts

---
+++ scripts/hello_world_prateekguptaiiitk.cpp
@@ -0,0 +1,14 @@
+// LANGUAGE: Cpp
+// AUTHOR: Prateek Gupta
+// GITHUB: https://github.com/prateekguptaiiitk
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	char hello[] = "Hello, World!";
+	printf("%s\n",hello);
+	return 0;
+}
