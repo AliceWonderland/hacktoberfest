66c513b2b7eff0c04bb37f0cbfbd3ac8d2e3ac9d
Prateek Gupta <prateekgupta@Prateeks-MacBook-Air.local>
2018-10-01 07:19:48 +0530

    Updated scripts

---
+++ scripts/hello_world_prateekguptaiiitk.swift
@@ -0,0 +1,11 @@
+// LANGUAGE: Swift
+// AUTHOR: Prateek Gupta
+// GITHUB: https://github.com/prateekguptaiiitk
+
+import UIKit
+
+var message = "Hello, World!"
+for i in message{
+    print(i, terminator:"")
+}
+
