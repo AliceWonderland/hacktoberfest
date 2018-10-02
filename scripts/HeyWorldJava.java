3d0293f74db460164995c2707334c5b09e257d7a
Sokratis Fotkatzikis <sokratis12GR@users.noreply.github.com>
2018-10-02 10:08:44 +0300

    Create HeyWorldJava.java

---
+++ scripts/HeyWorldJava.java
@@ -0,0 +1,12 @@
+// Author: Sokratis Fotkatzikis
+// GitHub: https://github.com/sokratis12GR
+// Language: Java
+
+public class HeyWorldJava {
+  
+  public static String welcome = "Hey World"
+  
+  public static void main() {
+    System.out.println(welcome);
+  }
+}
