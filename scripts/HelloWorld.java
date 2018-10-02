87a8787436a4b9fbb94062ce5acabc00ebda5ed3
Yudhst <43778886+Yudhst@users.noreply.github.com>
2018-10-02 17:37:43 +0700

    Create HelloWorld.java

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// AUTHOR: Naufal Yudhistira
+// GITHUB: https://github.com/yudhst
+
+
+public class  HelloWorld{
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+}
