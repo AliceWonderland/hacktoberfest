5b209283f90dae7418f8f21f5182ae95d7243a89
Sharanya Kamath <sherrykamath@gmail.com>
2018-10-02 19:23:51 +0530

    Create HelloWorld.cpp

---
+++ scripts/HelloWorld.cpp
@@ -0,0 +1,10 @@
+// LANGUAGE: C++
+// AUTHOR: Sharanya Kamath
+// GITHUB: https://github.com/sharanyakamath
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  cout<<"Hello World!"<<endl;
+  return 0;
+}
