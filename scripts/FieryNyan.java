58874e4b6029d67fb0c05af0bb4fe19685200e99
FieryNyan <maxteh777@gmail.com>
2018-10-02 10:33:39 +0800

    Create FieryNyan.java

---
+++ scripts/FieryNyan.java
@@ -0,0 +1,13 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: FieryNyan
+  GITHUB: https://github.com/FieryNyan
+*/
+
+public class FieryNyan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
