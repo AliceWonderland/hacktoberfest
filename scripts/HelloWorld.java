69019d33b0be53039e28c61f0995a4cd52d7f34b
singloon <kalanleung@outlook.com>
2018-10-03 20:17:06 +0100

    Addition of hello world in Java

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Ka Lan
+// GITHUB: https://github.com/singloon
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
