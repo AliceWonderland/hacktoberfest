a72f854b81a3e2fccc6f3e788e7d308ebce9b49e
Mr.Coder <34159717+viditvarshney@users.noreply.github.com>
2018-10-03 20:21:11 +0530

    Create hello_world_viditvarshney.ino

---
+++ scripts/hello_world_viditvarshney.ino
@@ -0,0 +1,8 @@
+// LANGUAGE: Arduino 
+// AUTHOR: Vidit Varshney
+// GITHUB: https://www.github.com/viditvarshney
+
+void setup(){
+        Serial.begin(9600);
+        Serial.println("Hello world!");
+}
