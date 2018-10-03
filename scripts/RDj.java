9cf9a62c9c3ce5fcb431b525bd95021c88e39282
RedDemptr <RedDemptr@users.noreply.github.com>
2018-10-03 13:42:27 -0500

    Create RDj.java

---
+++ scripts/RDj.java
@@ -0,0 +1,11 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: RedDemptr
+  GITHUB: https://github.com/RedDemptr
+*/
+
+public class RDj {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
