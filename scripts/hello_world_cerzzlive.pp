5ecca4550491cc9c5d543c7a556c78c3c0bb1e45
cerzzlive <cerzzlive@gmail.com>
2018-10-04 06:40:46 +0700

    Add Hello World in Pascal

---
+++ scripts/hello_world_cerzzlive.pp
@@ -0,0 +1,11 @@
+// LANGUAGE: Pascal
+// ENV: FPC 3.0.0
+// AUTHOR: Phattarajul Chantaramontree
+// GITHUB: https://github.com/cerzzlive
+
+program HelloWorld;
+
+begin
+    writeln('Hello, world!');
+end.
+
