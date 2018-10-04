3967cd8dd588288d7f2b3da65ad7e0e801dfeb9a
bjellesma <william.jellesma@gmail.com>
2018-10-03 23:13:09 -0400

    bjellesma hello world script

---
+++ scripts/hello_world_bjellesma.py
@@ -0,0 +1,7 @@
+#Language: Python
+#Env: Atom text editor (https://atom.io/)
+#Author: bjellesma
+#Github: https://github.com/bjellesma
+#Tutorial: thenewboston (https://www.youtube.com/watch?v=HBxCHonP6Ro&list=PL6gx4Cwl9DGAcbMi1sH6oAMk4JHw91mC_)
+
+print("Hello, World!")
