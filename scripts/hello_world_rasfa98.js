7730ffcf41e6e071b69adb51d6ac9f8c0c8d53b0
Rasmus Falk <rf222fu@student.lnu.se>
2018-10-04 12:45:53 +0200

    Add new hello world script

---
+++ scripts/hello_world_rasfa98.js
@@ -0,0 +1,10 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Rasmus Falk
+// GITHUB: https://github.com/rasfa98
+// LINK: https://eloquentjavascript.net/
+
+const name = 'rasfa98';
+const message = 'Hello, World!';
+
+console.log(name + ' yells ' + message.toUpperCase());
