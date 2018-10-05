28a0e0742bf46270d46c5135af51c9b84721c4da
Himani Sharma <himanisharma2304@gmail.com>
2018-10-05 13:28:44 +0530

    hello_world

---
+++ scripts/hello_world_himani_sharma.java
@@ -0,0 +1,13 @@
+// LANGUAGE: Java
+// ENV: JDK
+// AUTHOR: Himani Sharma
+// GITHUB: https://github.com/hsharma2304
+
+public class Java {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
