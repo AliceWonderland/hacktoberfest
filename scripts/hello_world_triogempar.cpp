de11bbc5472e3f591b9e973244f6f7fe10cd621e
Trio Gempar Surya Giri <suryagiri200@gmail.com>
2018-10-06 19:03:13 +0700

    Create hello_world_triogempar.cpp

---
+++ scripts/hello_world_triogempar.cpp
@@ -0,0 +1,12 @@
+// LANGUAGE: C++
+// AUTHOR: Trio Gempar Surya Giri
+// GITHUB: https://github.com/triogempar
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!";
+    return 0;
+}
