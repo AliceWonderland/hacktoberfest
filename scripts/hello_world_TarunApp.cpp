e90761180552521e2442012ab4c537ef8acbb3e5
TarunApp <tappannagari@gmail.com>
2018-10-06 14:09:32 -0700

    Added TarunApp Hello World File

---
+++ scripts/hello_world_TarunApp.cpp
@@ -0,0 +1,16 @@
+// Language: C++
+// ENV: C++, .NET, Windows
+// AUTHOR: Tarun Appannagari
+// GITHUB: https://github.com/TarunApp
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+cout << "Hello World!" << endl;
+
+return 0;
+
+}
