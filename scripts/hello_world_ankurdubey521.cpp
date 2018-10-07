ee1e7ce7eedf836a4d5e09783084b36bf5df81b9
Ankur Dubey <ankurdubey521@protonmail.com>
2018-10-08 00:10:40 +0530

    Add hello world program

---
+++ scripts/hello_world_ankurdubey521.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// ENV: linux
+// AUTHOR: Ankur Dubey
+// GITHUB: https://github.com/ankurdubey521
+
+#include<iostream>
+
+int main(){
+	std::cout << "Hello World";
+	return 0;
+}
