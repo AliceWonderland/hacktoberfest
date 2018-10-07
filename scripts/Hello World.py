5d6efb2414b2bb76b725485aecf2aada459c6af3
Nikhil Taneja <taneja.nikhil03@gmail.com>
2018-10-07 18:22:23 +0530

    Update Hello World.py

--- scripts/Hello World.py
+++ scripts/Hello World.py
@@ -1,6 +1,7 @@
+from __future__ import print_function
 #Lang Python2
 #env Interactive as well as script
 #aut Anshu Musaddi
 #github https://github.com/anshucollege
-print "Hello World"
-print "Kevin was here to Say Hello World!"
+print ("Hello World")
+print ("Kevin was here to Say Hello World!")
