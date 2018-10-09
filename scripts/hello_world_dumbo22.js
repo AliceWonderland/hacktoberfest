1c23df6826abb728eb2a45af4cfa4d142d71b28c
dumbo22 <hehe221093@gmail.com>
2018-10-09 11:26:51 +0530

    Created Helloworld

---
+++ scripts/hello_world_dumbo22.js
@@ -0,0 +1,9 @@
+// Language: JavaScript
+// ENV: Node.js
+// Author: Dumbo22
+// Github: https://github.com/dumbo22
+var helloworld = function() {
+  console.log('Hello, World!')
+}
+
+helloworld();
