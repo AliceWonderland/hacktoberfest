5d0d4b14a2f4d7b06b2ab27560c9eb70b5be5f3a
Endrik Lelo <endrik.lelo@nsgroup.nl>
2018-10-10 01:03:50 +0200

    Updated comment into helloworld script

--- scripts/HelloWorldEndrikLelo.java
+++ scripts/HelloWorldEndrikLelo.java
@@ -1,6 +1,7 @@
-//Author: Endrik Lelo
-//https://github.com/EndrikLelo
-//Language: Java
+// LANGUAGE: Java
+// ENV: JVM
+// AUTHOR: Endrik Lelo
+// GITHUB: https://github.com/EndrikLelo
 public class HelloWorldEndrikLelo{
 	public static void main(String args[]){
 		System.out.println("Hello World");
