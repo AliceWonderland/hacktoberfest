6ef1be3de85682f4adff926368065e3fc6d2511f
Jonas Karg <jonas.karg@hotmail.com>
2018-10-09 19:45:57 +0200

    Added my script
    Hotfix.

---
+++ scripts/hello_world_KargJonas.js
@@ -0,0 +1,9 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Jonas Karg
+// GITHUB: https://github.com/KargJonas
+
+console.log( ( () => {
+  let _$ = 111, $_ = Math.floor( Math.PI ), _$_ = $$ => String.fromCharCode( $$ ), $_$ = $ => $.join( String() )
+  return $_$( [Math.floor( _$ / 2 ) + ( $_ - 1 ) * 11 - 5, _$ - 20 / 5 * 2.5, _$ - $_, _$ - $_, _$, 32, 87, _$, _$ + $_, _$ - $_, _$ - 11, ( _$ - Math.pow( 10, 2 ) ) * 3].map( $ => _$_( $ ) ) )
+} )() );
