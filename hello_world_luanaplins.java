129c0887a30477e359ef24c3325078ff4e2c0746
Luana Brasil <luanaplins@gmail.com>
2018-10-08 23:28:28 -0300

    Added Java Hello_World

---
+++ hello_world_luanaplins.java
@@ -0,0 +1,9 @@
+// Language : Java
+// AUTHOR : Luana Lins
+// GITHUB : https://github.com/luanaplins
+
+class Hello_World{
+   public static void main(String[] args) {
+	System.out.println("Hello,World!");
+	}
+}
