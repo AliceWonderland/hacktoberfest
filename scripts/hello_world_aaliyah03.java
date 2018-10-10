c5fe7d0c2b913df8ab7f3712cbb09f3ef99174ba
Aaliyah Ahmed <43817581+aaliyah03@users.noreply.github.com>
2018-10-10 23:37:45 +0530

    Create hello_world_aaliyah03.java

---
+++ scripts/hello_world_aaliyah03.java
@@ -0,0 +1,12 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Aaliyah Ahmed
+  GITHUB: https://github.com/aaliyah03
+*/
+
+public class First {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello world!!:D");
+    }
+}
