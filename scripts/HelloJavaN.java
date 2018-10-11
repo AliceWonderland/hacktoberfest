2f354e843f7baf646ed1dcf72c18ec721748829e
Nikhilkumar Shivaji Kekan <nikhil19kekan@gmail.com>
2018-10-10 20:22:17 -0500

    Update HelloJavaN.java

--- scripts/HelloJavaN.java
+++ scripts/HelloJavaN.java
@@ -5,6 +5,7 @@
 */
 
 public class HelloJavaN {
+    //static main function which allows calling this method without creating object of it
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
