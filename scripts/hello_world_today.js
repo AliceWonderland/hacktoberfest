0b82a169bae19692d44fb274f363131a98e75e39
youlan <yibaiqiu@126.com>
2018-10-12 11:01:39 +0800

    add script hello_world_today.js

---
+++ scripts/hello_world_today.js
@@ -0,0 +1,7 @@
+/* LANGUAGE: Javascript
+ * ENV: Node.js
+ * AUTHOR: Alex Qiu
+ * GITHUB: https://github.com/youlanstudio
+ */
+
+console.log(`Today is ${new Date()}, We say hello to the new world!`)
