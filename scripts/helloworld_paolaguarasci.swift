acbc6b2660d2f9eb18600b5fe93cb008b4cd95bd
Paola Guarasci <paolaguarasci@gmail.com>
2018-10-12 07:45:29 +0200

    my awesome contrib

---
+++ scripts/helloworld_paolaguarasci.swift
@@ -0,0 +1,13 @@
+// LANGUAGE: Swift
+// ENV: Apple Swift version 4.2 (swiftlang-1000.0.29.2 clang-1000.10.39) @ x86_64-apple-darwin18.0.0
+// AUTHOR: Paola Guarasci
+// GITHUB: https://github.com/paolaguarasci
+//
+// This is a pretty simple "first script" in Swift
+//
+// How to run (work only on Mac)
+// $ swift helloSwiftWorld.swift
+//
+
+let myString: String = "Hello World"
+print(myString)
