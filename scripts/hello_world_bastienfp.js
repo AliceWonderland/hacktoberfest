76585299458abd1387f61981b8aea6e40e3f1813
bastienfp <bastien.fp@gmail.com>
2018-10-12 10:33:24 -0400

    Created hello_world_bastienfp.js

---
+++ scripts/hello_world_bastienfp.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Bastien Fournier-Peyresblanques
+// GITHUB: https://github.com/bastienfp
+
+console.log('Hello, World!');
+console.log('Thank you AliceWonderland for the opportunity to start contributing to the Open Source community!');
