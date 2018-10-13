535481dc8da874e06990ffb3124f52db35552041
Guillermo Ortiz Vitali <ortiz.vitali.guillermo@gmail.com>
2018-10-13 18:45:17 -0300

    My commit

---
+++ scripts/hello_world_guilleortiz.cs
@@ -0,0 +1,17 @@
+// LANGUAGE: C#
+// ENV: .Net Core
+// AUTHOR: Guillermo Oritz
+// GITHUB: https://github.com/guilleortiz
+
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
