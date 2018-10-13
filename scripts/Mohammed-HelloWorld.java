146380bb56e22a0b04bf1cbbfd110affb992c627
Mohammed <m.m.akinbayo@gmail.com>
2018-10-13 17:44:13 -0400

    Last commit. Added script, used java.

---
+++ scripts/Mohammed-HelloWorld.java
@@ -0,0 +1,12 @@
+// LANGUAGE: Java
+// AUTHOR: Mohammed
+// GITHUB: https://github.com/Mohammed532
+
+public class Mohammed-HelloWorld{
+	String x = "Hello";
+	String y = "World";
+
+	public static void main(String[] args) {
+		System.out.println(x + y);
+	}
+}
