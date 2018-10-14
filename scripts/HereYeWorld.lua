f2efb07f7220e97ce914e2aae3e047fd302ce128
Voperak <40292751+Voperak@users.noreply.github.com>
2018-10-14 11:54:35 -0400

    Create HereYeWorld.lua
    Just a simple hello/goodbye in pirate language.

---
+++ scripts/HereYeWorld.lua
@@ -0,0 +1,15 @@
+local function sayHello()
+
+  print("Arrghhhh, you there matey! Mornings to ya and may you be blessed with many pirate treasures!")
+
+end
+
+
+local function sayGoodbye()
+
+  print("Avast! I must be going to plunder!")
+
+end
+
+sayHello()
+sayGoodbye()
