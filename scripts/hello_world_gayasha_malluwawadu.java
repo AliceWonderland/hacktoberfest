fc8051ed7f9f8651555fb0169918d6b3b0248a8b
Gayasha Malluwawadu <36964262+Gyshie@users.noreply.github.com>
2018-10-14 12:58:23 +0530

    gayasha_malluwawadu.java

---
+++ scripts/hello_world_gayasha_malluwawadu.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Gayasha Malluwawadu
+// GITHUB: https://github.com/Gyshie
+
+public class Hello{
+public static void main (String []args){
+System.out.println("Hello World!");
+}
+}
