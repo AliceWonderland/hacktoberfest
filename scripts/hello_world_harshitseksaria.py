4dd370ff3eec122e201d6d625188a54810b44315
Harshit Seksaria <37345795+HarshitSeksaria@users.noreply.github.com>
2018-10-14 20:22:37 +0530

    Created hello_world_harshitseksaria.py
    Here is a funny way to print 'Hello World' using Python 3.

---
+++ scripts/hello_world_harshitseksaria.py
@@ -0,0 +1,8 @@
+# LANGUAGE: Python
+# AUTHOR: Harshit Seksaria (aka DudeCoder)
+# GITHUB: https://github.com/HarshitSeksaria
+
+toBePrinted = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
+
+for ch in toBePrinted:
+  print(ch, end='')
