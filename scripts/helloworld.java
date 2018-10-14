62cce1f92b6c2d3f97ef9cdcded45c79d871a3f7
Akshay <akshaychopra96@gmail.com>
2018-10-14 14:41:52 +0530

    first contribution

---
+++ scripts/helloworld.java
@@ -0,0 +1,11 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Akshay Chopra
+  GITHUB: https://github.com/akshaychopra96
+*/
+
+public class Hellow {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
