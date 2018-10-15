b3115b1b0c1c684bd049ae0abddb628a79ec95ec
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-10-15 18:36:18 +0530

    Create hello_world_seepls.java

---
+++ scripts/hello_world_seepls.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java 
+// AUTHOR: Smriti Tiwari
+// GITHUB: https://github.com/seepls
+
+class Hello{
+public static void main(String args[]){
+ System.out.print("Hello, World!");
+}
+}
