ba1bb8646fd5fe4735d2ee316dde993d4008e073
Edward Feng <feng.dingding@gmail.com>
2018-10-15 13:25:22 -0400

    yet another hello world program

---
+++ scripts/helloworld_edwfeng.java
@@ -0,0 +1,11 @@
+// LANGUAGE: Java
+// Author: Edward Feng
+// Github: https://www.github.com/edwfeng
+
+// YAHWP (?): Yet Another Hello World Program
+
+public class hacktoberfest{
+    public static void main(String[] args) {
+        System.out.println("Helllo World");
+    }
+}
