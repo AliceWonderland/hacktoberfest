69ae25883d44790a32653477c03e9c2282c0e886
Alexius Adhitya K <debugvelop@gmail.com>
2018-10-15 15:39:05 +0700

    Create Hello_World_AlexiusAdhityaK.cpp

---
+++ scripts/Hello_World_AlexiusAdhityaK.cpp
@@ -0,0 +1,13 @@
+// LANGUAGE: C++
+// ENV: VS Code
+// AUTHOR: Alexius Adhitya K
+// GITHUB: https://github.com/debugvelop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World! << endl;
+  return 0;
+}
