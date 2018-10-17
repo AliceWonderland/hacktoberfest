08fc9a09b0b1b294abfc9f13ad880480d01f3ebb
fahri314 <fahri314@gmail.com>
2018-10-17 12:29:54 +0300

    add user

---
+++ scripts/scrolling_hello_fahri314.py
@@ -0,0 +1,30 @@
+# LANGUAGE: python
+# ENV: /usr/bin/env
+# AUTHOR: Fahri Guresci
+# GITHUB: https://github.com/fahri314
+
+import sys
+from time import sleep
+import os
+
+#print("\033[y coordinate;[x coordinateH Hello")
+os.system('cls')
+sleep(0.2)
+print("\033[1;1HHello World!")
+sleep(0.2)
+print("\033[1;1H  Hello World!")
+sleep(0.2)
+print("\033[1;1H    Hello World!")
+sleep(0.2)
+print("\033[1;1H      Hello World!")
+sleep(0.2)
+print("\033[1;1H        Hello World!")
+sleep(0.2)
+print("\033[1;1H      Hello World!")
+sleep(0.2)
+print("\033[1;1H    Hello World!")
+sleep(0.2)
+print("\033[1;1H  Hello World!")
+sleep(0.2)
+print("\033[1;1HHello World!")
+sleep(0.2)
