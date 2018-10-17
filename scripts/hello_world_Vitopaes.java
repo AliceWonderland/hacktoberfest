da1465d2ec6ba47c5f88d66be6ea5d8e63da18c8
Vitor Paes <vitor.mor66@live.com>
2018-10-17 00:16:44 -0300

    Create hello_world_Vitopaes.java

---
+++ scripts/hello_world_Vitopaes.java
@@ -0,0 +1,21 @@
+// LANGUAGE: Java
+// ENV: NetBeans
+// AUTHOR: Vitor Paes
+// GITHUB: https://github.com/VitorPaes
+
+package hello_world;
+
+/**
+ *
+ * @author Vitor
+ */
+public class Hello_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+    
+}
