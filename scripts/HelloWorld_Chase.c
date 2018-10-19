233f109613bb573521cc77d8ce1b7163a68cc850
Rajdeep Maity <44284917+TotallyNotChase@users.noreply.github.com>
2018-10-19 11:58:52 +0530

    Created a script for HelloWorld in C
    Created a Hello World program in C and added information about author.

---
+++ scripts/HelloWorld_Chase.c
@@ -0,0 +1,11 @@
+// Language: C
+// ENV : Visual Studio
+// AUTHOR : Rajdeep Maity
+// GITHUB : https://github.com/TotallyNotChase
+
+#include<stdio.h>
+
+void main()
+{
+	printf("Hello World!\n");
+}
