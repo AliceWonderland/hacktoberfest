0612153b4f75522f02d4ccf0da016c390a58ea6a
rahul mishra <misrarahul1100@gmail.com>
2018-10-21 22:18:37 +0530

    created hello word in js by rahul mishra

---
+++ scripts/helloword11.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<script>
+function hello()
+{
+alert("Hello World!");
+}
+</script>
+</head>
+<body>
+<button onclick="hello()">Click Me</button></br></br>
+</body>
+</html>
