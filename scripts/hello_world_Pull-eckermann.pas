66ecaf63b58a57a660876340316317884f6bbcc5
Pull-eckermann <erick7880@gmail.com>
2018-10-23 11:36:55 -0200

    tanks for this opportunity, i creat a Hello world in pascal

---
+++ scripts/hello_world_Pull-eckermann.pas
@@ -0,0 +1,11 @@
+//LANGUAGE: Pascal
+//AUTOR: Erick Eckermann Cardoso
+//GITHUB: https://github.com/Pull-eckermann
+
+program HelloWord;
+begin
+	writeln('Hello, world!');
+end.
+
+//Pascal is a coll language, but sometimes it's very boring...
+//Lockout for this precouser of computer language
