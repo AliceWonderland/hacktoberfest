c215c1848a438b8b882d7ae035a1e13671762e8d
Mike9354 <M-ramsden@live.co.uk>
2018-10-24 13:23:44 +0100

    Update scripts/Hello World C#.cs
    C# Hello world

---
+++ scripts/Hello World C#.cs
@@ -0,0 +1,20 @@
+/** 
+* LANGUAGE: C#
+* AUTHOR: Michael Ramsden
+* GITHUB: https://github.com/mike9354**/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world! :)");
+            Console.ReadLine();
+        }
+    }
+}
