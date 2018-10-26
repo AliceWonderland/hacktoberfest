4eb3e9f22af0e2c9686aca7e8380d4eb4c442c41
Michal Drahorat <michaldrahorat@gmail.com>
2018-10-26 00:57:06 -0400

    3. Hello World Script

---
+++ scripts/mdcodes_helloworld_csharp.cs
@@ -0,0 +1,10 @@
+/*  Name: mdcodes
+	Date Created: 10/26/2018
+*/
+namespace HelloWorld{
+	public class HelloWorld{
+		static void main(string[] args){
+			console.WriteLine("Hello World!");
+		}
+	}
+}
