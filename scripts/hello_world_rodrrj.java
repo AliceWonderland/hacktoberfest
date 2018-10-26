f542d39e6aa4ad0e8cfe258fb72c27d76ef2e4c6
Justin R <jrod1199@gmail.com>
2018-10-26 17:02:45 -0500

    Custom Hello World script.
    Was written in Java through jGrasp.

---
+++ scripts/hello_world_rodrrj.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+
+   public static void main(String[] args)
+   {
+   
+   System.out.println("Hello, World!");
+   
+   }
+    
+}
