1aca0c4f3464cefdd753acde81222ba375307305
kavishkarajakaruna <kavishkanccmaths@gmail.com>
2018-10-27 13:18:37 +0530

    The pull request from KR

---
+++ scripts/hello_KavishkaRajakaruna.js
@@ -0,0 +1,6 @@
+// LANGUAGE: JavaScript
+// ENV: Node.js
+// AUTHOR: Kavishka Rajakaruna
+// GITHUB: https://github.com/KavishkaRajakaruna
+
+console.log(“Hello World! I’m Kavishka Hirushan Rajakaruna,I'm a student that is interrested in robotics , machine learing & deep learning”);
