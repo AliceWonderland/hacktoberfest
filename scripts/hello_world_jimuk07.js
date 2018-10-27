9b37fe118efc80f308602053ab22e87a8bd76736
James Bowler <jimuk07@yahoo.co.uk>
2018-10-27 23:16:55 +0100

    add J Bowler plus scripts

---
+++ scripts/hello_world_jimuk07.js
@@ -0,0 +1,8 @@
+const a = "Hello";
+const b = "World";
+
+function hello(x, y) {
+  return x + " " + y + "!";
+}
+
+console.log(hello(a, b));
---
+++ scripts/hello_world_jimuk07.py
@@ -0,0 +1,12 @@
+a = "Hello"
+b = "World"
+
+
+def hello(x, y):
+    return x + " " + y + "!"
+    
+
+print(hello(a, b))
+
+
+
