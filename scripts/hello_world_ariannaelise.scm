18c2cc711d688c47f99537eae29aa8b171eb2cb4
Arianna Scott <ariannaelise@hotmail.com>
2018-10-27 20:11:05 -0400

    added hello world, written in Scheme

---
+++ scripts/hello_world_ariannaelise.scm
@@ -0,0 +1,7 @@
+; Language: Scheme
+; ENV: MIT-Scheme (R5RS)
+; Author: ariannaelise
+; Github: https://github.com/ariannaelise/
+(begin
+  (display "Hello, world!" (current-output-port))
+  (newline (current-output-port)))
