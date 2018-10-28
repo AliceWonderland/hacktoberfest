2d85f91a815a90754fcf3565561f3dc9a608a74d
Zeeshan <43438385+15bcs019@users.noreply.github.com>
2018-10-28 16:15:48 +0530

    created helloworld script hasktoberfest

---
+++ scripts/helloworld_15bcs019.hs
@@ -0,0 +1,8 @@
+-- LANGUAGE: Haskell
+-- AUTHOR: 15bcs019
+-- GITHUB: https://github.com/15bcs019
+-- COMPILE: ghc --make hello_world_bareon.hs -o hello
+module Main where
+
+main :: IO ()
+main = putStr "Hello World!!"
