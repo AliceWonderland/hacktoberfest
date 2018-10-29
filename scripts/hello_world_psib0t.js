c1840e48e1be1047971fc982abd6665dd7033b1f
Arvind Narayanan <arvindonarayanan@gmail.com>
2018-10-29 19:43:47 +0530

    Feat: My entry

---
+++ scripts/hello_world_psib0t.js
@@ -0,0 +1,13 @@
+/* LANGUAGE: JavaScript
+ * ENV: Node.js
+ * AUTHOR: PSIB0T
+ * GITHUB: https://github.com/psib0t
+ */
+
+
+ let hour = new Date().getHours();
+
+ if (hour < 12)
+    console.log("Good morning");
+else
+    console.log("Good evening");
