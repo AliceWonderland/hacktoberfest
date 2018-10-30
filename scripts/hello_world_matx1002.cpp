50c0bb9ccf5c192e88a7fa2483c3922083617ee4
matx1002 <35429241+matx1002@users.noreply.github.com>
2018-10-30 18:34:56 +0100

    created my hello world

---
+++ scripts/hello_world_matx1002.cpp
@@ -0,0 +1,13 @@
+// LANGUAGE: C++
+// AUTHOR: matx1002
+// GITHUB: https://github.com/matx1002
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
