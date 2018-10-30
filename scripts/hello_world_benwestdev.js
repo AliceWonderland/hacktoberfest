ee60aea2c01182e9d82dbbc4d378abea07f75dbc
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2018-10-30 07:33:39 -0400

    add hello world script

---
+++ scripts/hello_world_benwestdev.js
@@ -0,0 +1,11 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Benwestdev
+// GITHUB: https://github.com/benwestdev
+
+
+var greeting = function(name) {
+  console.log('Hello, ' + name);
+}
+
+greeting('benwestdev');
