03fe1b41f270d45741aa57f36220ee1c7c3b429a
Sufia Almas <sufiaalmas4@gmail.com>
2018-10-30 21:17:55 +0530

    corrected

--- scripts/hello_world_sufia.js
+++ scripts/hello_world_sufia.js
@@ -1,6 +1,6 @@
 // LANGUAGE: JavaScript
 // ENV: Node.js
 // AUTHOR: SUFIA ALMAS
-// GITHUB: https://github.com/manishmittal9
+// GITHUB: https://github.com/sufiaalmas
 
 console.log(“Hello World! I’m Sufia Almas”);
