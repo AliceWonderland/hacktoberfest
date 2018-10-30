290659068014bc21d61a2d59a6005fdb99f458f8
asdf123asdf123 <44621578+asdf123asdf123@users.noreply.github.com>
2018-10-30 15:24:22 -0500

    Create asdf123asdf123.java

---
+++ scripts/asdf123asdf123.java
@@ -0,0 +1,13 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: asdf123asdf123
+  GITHUB: https://github.com/asdf123asdf123
+*/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
