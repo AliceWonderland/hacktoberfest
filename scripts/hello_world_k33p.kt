e496fd1c8236189208297dad95f3d94617dff2d0
k33p <k33p-mac@k33ps-MacBook-Pro.local>
2018-10-31 21:17:13 +0100

    Added hello world

---
+++ scripts/hello_world_k33p.kt
@@ -0,0 +1,8 @@
+// LANGUAGE: Kotlin
+// ENV: Kotlin JVM
+// AUTHOR: Dmitry Gaynullin
+// GITHUB: https://github.com/GaynullinDima/
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
