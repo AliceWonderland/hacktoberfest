89c1fdeeae2841da4dff794dcb7948ba429fa967
Marysol <mlosa001@gmail.com>
2018-10-31 19:49:35 -0400

    added link

--- scripts/hello_world_mlosa001.js
+++ scripts/hello_world_mlosa001.js
@@ -2,7 +2,7 @@
 //ENV: Node.js
 //AUTHOR: Marysol Losada
 //GITHUB: https://github.com/mlosa001
-//LINK:https://www.w3schools.com/js/
+//LINK:https://www.w3schools.com/js
 
 
 console.log('Hello, World!');
