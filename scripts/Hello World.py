cad1a1085d5c3d096c1697876d5a4e185d0f0451
SoulTaku <zombie2912@yahoo.com>
2018-10-31 20:38:36 +0200

    tried a different method of printing

--- scripts/Hello World.py
+++ scripts/Hello World.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python
 #Lang Python2
 #env Interactive as well as script
 #aut Anshu Musaddi
 #github https://github.com/anshucollege
+import sys
 print "Hello World"
 print "Kevin was here to Say Hello World!"
+sys.stdout.write('R$A is better than AE$')
+sys.stdout.flush()
