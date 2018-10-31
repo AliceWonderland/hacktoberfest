894ee6d65f9c1e28cf357a0d4eb70f8d064fbf6a
Jackson Silva <jackson.victorius@gmail.com>
2018-10-31 19:20:24 -0300

    Create Jack's-HelloWorld.cs

---
+++ scripts/Jack's-HelloWorld.cs
@@ -0,0 +1,23 @@
+using System;
+
+class HELL_O
+{
+    static void Main(string[] args)
+    {
+        Console.ReadKey();
+        Console.WriteLine("Hello World!");
+        //Console.ReadKey();
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Hello World!");
+        //Console.ReadKey();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Hello World!");
+        Console.ReadKey();
+        Console.ResetColor();
+        Console.WriteLine();
+        Console.WriteLine("Acabou...");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
