1dbf6562cfeaa12469217e83f892305094dc088e
ibrahimm02 <44142040+ibrahimm02@users.noreply.github.com>
2018-11-19 22:56:06 +0000

    Added hello world
    Hello World Java Program

---
+++ scripts/HelloWorld_Ibrahim.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// AUTHOR: Ibrahim Mathivanan
+// GITHUB: https://github.com/ibrahimm02
+
+public class HelloWorld_Ibrahim {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! I'm Ibrahim");
+    }
+}
