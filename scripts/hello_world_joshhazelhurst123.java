69000d75174e18500f03bfac428b2c576b82f131
Josh <joshhazelhurst123@gmail.com>
2019-02-16 17:30:15 +1300

    Create hello_world_joshhazelhurst123.java

---
+++ scripts/hello_world_joshhazelhurst123.java
@@ -0,0 +1,12 @@
+
+//Language : Java
+// Author : joshhazelhurst123
+//GitHUb Profile: https://github.com/joshhazelhurst123
+
+
+public class GitHub_Hacktoberfest{
+	
+		public static void main(String[] args){
+			System.out.println("Hello, World!");
+		}
+}
