d70f48e84a23eb71cdc9d3f2b11204228dd98949
Vireak <44746913+Vireak-Sok@users.noreply.github.com>
2019-03-05 16:21:39 +0700

    Update helloWorld.c

--- scripts/helloWorld.c
+++ scripts/helloWorld.c
@@ -1,4 +1,9 @@
-#include<stdio.h>
+// LANGUAGE: C
+// ENV: C.c
+// AUTHOR: Vireak Sok
+// GITHUB: https://github.com/Vireak-Sok
+
+console.log('Hello, World!')#include<stdio.h>
 
 void main() {
     printf("Hello World");
