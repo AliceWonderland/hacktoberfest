15e96d81008d56659db1aae8ef833c158d3ddd37
Xeluh <55999674+Xeluh@users.noreply.github.com>
2019-09-30 15:20:56 -0400

    New files

---
+++ profiles/Alex_Alvarado.md
@@ -0,0 +1,13 @@
+# Alex Alvarado
+
+### Location
+
+Lakeland/USA
+
+### Academics
+
+Florida Poly
+
+### Profile Link
+
+[Xeluh](https://github.com/Xeluh)
---
+++ scripts/hello_world_Xeluh.cpp
@@ -0,0 +1,10 @@
+// LANGUAGE: C++
+// AUTHOR: Alex Alvarado
+// GITHUB: https://github.com/Xeluh
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
