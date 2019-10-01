20ae117712ab377f52a0eaa585ea0a0f47591941
ervaibhavkumar <vk530873@gmail.com>
2019-10-01 22:39:20 +0530

    Added all the tasks

---
+++ scripts/Hello_World_By_Vaibhav.py
@@ -0,0 +1,7 @@
+# Lang Python3
+# env Interactive as well as script
+# aut Vaibhav Kumar
+# github https://github.com/ervaibhavkumar
+
+print("Hello World")
+print("Guess Whatttt! Vaibhav was here to Say Hello World!")
