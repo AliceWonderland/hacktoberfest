9243795fc1d28a2261fb60a43e2566d9f5c156b4
Al Such <alwsuch@gmail.com>
2019-10-01 23:25:29 -0400

    Added my Hello, World script.

---
+++ scripts/helloworld_alsuch.java
@@ -0,0 +1,12 @@
+/* Language: JAVA
+Env: Atom
+Author: Al Such
+Github: https://github.com/ajaxironside */
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+
+  System.out.println("Hello, World!")
+  }
+}
