2e2f1a901e51ec552d3c411edfc05166ad4e1a7b
CaptainLazarus <adityagudimetla@gmail.com>
2019-10-02 21:50:30 +0530

    First Hacktoberfest Commit

---
+++ scripts/HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+
+Name : Aditya Sujith Gudimetla / CaptainLazarus
+github : https://github.com/CaptainLazarus
+Sunglasses: On  (☞⌐▀͡ ͜ʖ͡▀ )☞
+Hotel: Trivago
+
+*/
+
+#include<iostream>
+
+int main(){
+    std::cout << "Hello World" << "\n";
+    return 0;
+}
