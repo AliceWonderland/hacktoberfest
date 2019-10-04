6b2ead856dd402cedb1ae9b2bfc659f737c3a7c4
panjiad <panjiawwaludi430@gmail.com>
2019-10-04 23:20:19 +0700

    add-panjiAD

--- scripts/Hello_World_PanjiAD.js
+++ scripts/Hello_World_PanjiAD.js
@@ -2,5 +2,6 @@
 // ENV: Node.js
 // AUTHOR: Panji Awwaludi Dzkirawan
 // GITHUB: https://github.com/PanjiAD
+// LEARN : https://flaviocopes.com/javascript
 
 console.log("Hello, World!. I joined hacktoberfest :)");
