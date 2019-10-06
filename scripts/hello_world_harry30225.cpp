3804334f147235d40bb47473a22eac1911f1bcbe
harry30225 <harshit.s@students.iiit.ac.in>
2019-10-06 16:24:05 +0530

    hacktoberfest is fun and enjoyable kindly add me to the board . open source is fun.

---
+++ scripts/hello_world_harry30225.cpp
@@ -0,0 +1,11 @@
+
+//LANGUAGE:C++
+//GitHub=(https://github.com/harry3022https://)
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world";
+    cout<<endl;
+    return 0;
+}
