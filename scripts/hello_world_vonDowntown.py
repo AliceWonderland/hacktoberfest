6215c47c318e7e3a26d0ff8303f9132b12afb3de
vonDowntown <20665646+vonDowntown@users.noreply.github.com>
2019-10-06 17:41:17 +0200

    obfuscated version

--- scripts/hello_world_vonDowntown.py
+++ scripts/hello_world_vonDowntown.py
@@ -5,5 +5,5 @@
 
 #print("Hello World!")
 
-help = ("Hello {}!").format("World")
+help = "H{}".format("e{}".format("ll{}".format("o {}".format("W{}".format("o{}".format("r{}".format("l{1}{0}".format("!", "d"))))))))
 print(help)
