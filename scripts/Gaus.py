4231a8650d341120ef2ac3cf47c23b7efa3177ee
Artur <arturcastropolonorte@gmail.com>
2019-10-07 18:16:48 -0300

    Math Gaus

---
+++ scripts/Gaus.py
@@ -0,0 +1,7 @@
+total = 0
+for num in range(101):
+    #FORMULA DE GAUS mostra todas as somas de algarismos de 0 a 100
+    total = total + num
+    print(total)
+
+print("Resultado final da soma de 100: " + str(total))
