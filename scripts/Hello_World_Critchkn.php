7b3790d8c4f7b09b54a4e4f54d883cd809d5eb10
Andrew Elliott <mtgelliott15@gmail.com>
2019-10-09 13:49:30 -0500

    Added necessary files for HacktoberFest

---
+++ scripts/Hello_World_Critchkn.php
@@ -0,0 +1,17 @@
+// LANGUAGE: PHP
+// AUTHOR: Andrew Elliott
+// GITHUB: https://github.com/critchkn
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Hacktober Fest Hello World!</title>
+    </head>
+    <body>
+        <h1>
+            <?php echo 'Hello World!'; ?>
+        </h1>
+    </body>
+</html>
