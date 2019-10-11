0e25515cbc155554424fdc42fdb62c5f0b9b7156
Vanshika Jalan <vanshikajalan26@gmail.com>
2019-10-12 00:58:35 +0530

    adding cpp file

---
+++ scripts/forcpp.cpp
@@ -0,0 +1,17 @@
+/*Lang C++
+env script
+aut Vanshika Jalan
+github https://github.com/jalananshika*/
+#include<iostream.h>
+int main() 
+{ 
+    int i=0; 
+      
+    for (i = 1; i <= 10; i++) 
+    { 
+        printf( "Hello World\n");     
+    } 
+  
+    return 0; 
+} 
+Output:
