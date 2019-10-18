94535666b44f0b18743374188fa1177176da6bba
Keven Lefebvre <orditeck@gmail.com>
2019-10-18 10:52:52 -0400

    Update hello_world.php
    Make PSR2 compliant

--- scripts/hello_world.php
+++ scripts/hello_world.php
@@ -1,10 +1,9 @@
-<?php 
-/** 
+<?php
+/**
 * LANGUAGE: PHP
 * ENV: PHP/Apache/Nginx
 * AUTHOR: Junior Oliveira, Billy Lee
 * GITHUB: https://github.com/arojunior, https://github.com/leebilly0
 */
-echo "Hello World!";
 
-?>
+echo "Hello World!";
