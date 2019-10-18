ef8fd1116c15b3c489ecb9488b2f32c797b54576
Chinmay Chhajed <chinmay.chhajed@espressif.com>
2019-10-19 01:56:39 +0530

    Branch 3 for hacktoberfest!

---
+++ scripts/hello_world_cc.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+echo "Hello, Calander for 2019:"
+cal 2019
+echo "World is so beautiful, let's say hello to it!"
+sleep 1
+echo "3.."
+sleep 1
+echo "2.."
+sleep 1
+echo "1.."
+sleep 1
+echo "Hello World!!! :)
+"
