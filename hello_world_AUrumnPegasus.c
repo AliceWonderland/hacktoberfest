2fbff9936fccbbe2bcb3cc9d4bd0af7506b557e7
AurumnPegasus <54315149+AurumnPegasus@users.noreply.github.com>
2019-10-18 22:27:56 +0530

    Create hello_world_AUrumnPegasus.c

---
+++ hello_world_AUrumnPegasus.c
@@ -0,0 +1,10 @@
+//LANGUAGE: C
+//ENV: helloworld.c
+//AUTHOR: AurumnPegasus
+//GITHUB: https://github.com/AurumnPegasus
+#include <stdio.h>
+int main(void)
+{
+  printf("Hello, World!\n");
+  return 0;
+}
