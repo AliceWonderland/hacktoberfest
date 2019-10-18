f8c69a1f65192563119bbf82c7191b1dfa4aca34
Alexandre Velloso <xande.velloso@gmail.com>
2019-10-18 11:26:05 -0300

    Created a Hello World in Java

---
+++ scripts/hello_world_AlexandreVelloso.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// ENV: JVM
+// AUTHOR: Alexandre Velloso
+// GITHUB: https://github.com/AlexandreVelloso
+public class hello_world_AlexandreVelloso {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
