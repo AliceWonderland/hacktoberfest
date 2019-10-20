d7869097c7ad974254970ee077359fe1097c877d
sandeep030301 <sandeep030301@gmail.com>
2019-10-20 14:17:48 +0530

    Added Hello world

---
+++ scripts/Hello world.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ldb = long double;
+using pa = pair < ll, ll >;
+using vec = vector < ll >;
+#define pb push_back
+#define F  first
+#define S  second
+#define f(i,x,n) for(ll i=x;i<n;i++) 
+#define all(c) c.begin(),c.end()
+#define edl "\n"
+#define str string
+#define fast() ios_base::sync_with_stdio(false);cin.tie(NULL)
+const int MOD = 1e9 + 7, N = 1e5 + 10;
+const ll LINF = LLONG_MAX;
+int32_t main(){
+    fast();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout<<"Hello World !";
+    return 0;
+}
