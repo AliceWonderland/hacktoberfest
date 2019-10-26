e5b448a9a79aeae947e1bc91e43687d6b762755f
shadab khan <shadabkhan127@gmail.com>
2019-10-26 14:46:35 +0530

    hello_world_shadab_khan

---
+++ scripts/hello.cpp
@@ -0,0 +1,15 @@
+/**
+ * @Author: shadabKhan
+ * @Date:   Saturday, October 26th 2019, 2:43:19 pm
+ * @Last modified by:   shadabKhan
+ * @Last modified time: Saturday, October 26th 2019, 2:44:14 pm
+ */
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  cout<<"hello-world"<<endl;
+}
