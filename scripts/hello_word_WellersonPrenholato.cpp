0b3eca96fb5cee12fc1c60419a7baf9b8b043497
Wellerson Prenholato <wellerson1996@hotmail.com>
2019-10-26 18:34:02 -0300

    Script Wellerson Prenholato

---
+++ scripts/hello_word_WellersonPrenholato.cpp
@@ -0,0 +1,13 @@
+// IDIOMA: C++ 
+// AUTOR: Wellerson Prenholato
+// GITHUB: https://github.com/WellersonPrenholato
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+
+    cout << "Hello, Word!" << endl;
+    return 0;
+}
