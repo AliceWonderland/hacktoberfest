a8def1bb89a68bdc93e9d948cc68fca3049fad0f
Pratim14 <49002305+Pratim14@users.noreply.github.com>
2019-10-27 09:26:11 +0530

    Added comments for benificial understanding

--- scripts/Hello World.py
+++ scripts/Hello World.py
@@ -2,5 +2,5 @@
 #env Interactive as well as script
 #aut Anshu Musaddi
 #github https://github.com/anshucollege
-print "Hello World"
-print "Kevin was here to Say Hello World!"
+print "Hello World" #Just a start 
+print "Kevin was here to Say Hello World!" #To mark someone was here to contribute to the open source
