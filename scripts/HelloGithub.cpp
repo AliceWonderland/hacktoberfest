d7dd64367eba8f0c536f89c42870120d5353e880
Sourabh Kumar <45718852+Set78@users.noreply.github.com>
2019-10-29 15:21:45 +0530

    Create HelloGithub.cpp

---
+++ scripts/HelloGithub.cpp
@@ -0,0 +1,19 @@
+// LANGUAGE: C++
+// ENV: GNU
+// AUTHOR: Sourabh Kumar
+// GITHUB: https://github.com/Set78
+
+// This Hello Github is made in C++ language .
+
+
+
+//here is code
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Hello World"<<endl;
+  return 0;
+}
