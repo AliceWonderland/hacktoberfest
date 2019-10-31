b45b8828d3c1b84e1a47fadb628c6949686cf00c
diptags <rd.pradipta@gmail.com>
2019-10-31 13:42:53 +0700

    Add Hello World Script using C++

---
+++ scripts/hello_world_diptags.cpp
@@ -0,0 +1,14 @@
+// LANGUAGE: C++
+// AUTHOR: Pradipta
+// GITHUB: https://github.com/diptags
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	for(int i=0; i<5; i++){
+		cout << "Hello, World!" << endl;
+	}
+        return 0;
+}   
