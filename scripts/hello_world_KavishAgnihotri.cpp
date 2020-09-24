8d5c53b2f822b531ca4c6fcb77cf49d65b757f11
Kavish Agnihotri <kavish.agnihotri@gmail.com>
2020-09-24 13:56:13 +0530

    Kavish Agnihotri Commit

---
+++ scripts/hello_world_KavishAgnihotri.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Kavish Agnihotri
+// GITHUB: https://github.com/Kavish-Agnihotri
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World..!" << endl;
+	return 0;
+}
