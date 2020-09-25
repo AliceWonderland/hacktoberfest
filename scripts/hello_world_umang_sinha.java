912d50d41f379fff638ef45a58c616cb3b517d49
umang-sinha <umangsinha12@gmail.com>
2020-09-25 06:08:28 +0530

    Added a hello world script in java

---
+++ scripts/hello_world_umang_sinha.java
@@ -0,0 +1,10 @@
+//Language - Java
+//IDE - IntelliJ IDEA
+//Author - Umang Sinha
+//Github - https://github.com/umang-sinha/ 
+
+public class hacktoberfest {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
