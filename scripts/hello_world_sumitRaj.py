eb60ec8566169b7ea094da89f6796d07362b88c2
hiraj <hirajsumit@gmail.com>
2020-10-01 08:00:14 +0530

    Added profile Sumit-raj-au5'

--- scripts/hello_world_sumitRaj.py
+++ scripts/hello_world_sumitRaj.py
@@ -2,9 +2,9 @@
 # AUTHOR: Sumit Raj
 # GITHUB: https://github.com/sumit-raj-au5
 
-import sleep from time
+from time import sleep
 
 message = 'Hello World! I am tring to learn Python
 for ch in message:
 	print ch,
-	time.sleep(0.5)
+	time.sleep(0.5) 
