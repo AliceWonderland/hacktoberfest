b1c97c9ddbfbb372601a6280eadadd95dae83cab
Usman <muausman111@gmail.com>
2020-10-01 11:23:04 +0500

    Contribution from muhammadosmanali

---
+++ scripts/hello_world_muhammadosmanali.py
@@ -0,0 +1,29 @@
+# LANGUAGE: Python
+# ENV: Python 3
+# AUTHOR: Muhammad Usman Ali
+# GITHUB: https://github.com/muhammadosmanali
+
+import time
+
+if __name__ == "__main__":
+    print(f"H\r", end=" ")
+    time.sleep(0.4)
+    print(f"e\r", end="  ")
+    time.sleep(0.4)
+    print(f"l\r", end="    ")
+    time.sleep(0.4)
+    print(f"l\r", end="     ")
+    time.sleep(0.4)
+    print(f"o\r", end="      ")
+    time.sleep(0.4)
+    print(f" W\r", end="        ")
+    time.sleep(0.4)
+    print(f"o\r", end="         ")
+    time.sleep(0.4)
+    print(f"r\r", end="          ")
+    time.sleep(0.4)
+    print(f"l\r", end="           ")
+    time.sleep(0.4)
+    print(f"d\r", end="            ")
+    time.sleep(0.4)
+    print(f"\nHello World\r", end="")
