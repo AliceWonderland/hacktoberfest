d5f9be2a811b6b2692d1c72eb98a5180e8f912d9
Mahesh Jindal <mahesh.jindal6@gmail.com>
2020-10-01 21:49:22 +0530

    Added contribution info for MaheshJindal

---
+++ scripts/hello_world_maheshjindal.scala
@@ -0,0 +1,10 @@
+// LANGUAGE: Scala
+// ENV: Scala
+// AUTHOR: Mahesh Jindal
+// GITHUB: https://github.com/maheshjindal
+
+object HelloWorld {
+    def main(args: Array[String]) = {
+        println("Hello, world")
+    }
+}
