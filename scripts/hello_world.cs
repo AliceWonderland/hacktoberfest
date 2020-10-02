5b4bcbf405e792d26db4ceaa190e5cf5c7b83085
Christopher McDougal <cmcdougal@Flexcare.local>
2020-10-02 13:33:23 -0700

    fix

--- scripts/hello_world.cs
+++ scripts/hello_world.cs
@@ -9,7 +9,7 @@ namespace Hacktoberfest
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!`n");
         }
     }
 };
