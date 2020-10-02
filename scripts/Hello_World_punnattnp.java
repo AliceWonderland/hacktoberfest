9305906456279e8a2da4b20075eb184d7e82c5b7
Punnatt Punnotok <punnattnp@Punnatts-MacBook-Pro.local>
2020-10-03 02:25:58 +0700

    Hello_World_Java_punnattnp

---
+++ scripts/Hello_World_punnattnp.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Punnatt
+// GITHUB: https://github.com/punnattnp
+
+public class Hello_World_punnattnp{
+    public static void main(String args[]){
+        System.out.println("Hello, World!");
+    }
+}
