413515090a2d12cc42d7c4b002e142d053d3d11e
Yothin homjan <yothin.indy.8@gmail.com>
2020-10-03 17:16:59 +0700

    Create HelloWorld_inline.kt

---
+++ scripts/HelloWorld_inline.kt
@@ -0,0 +1,18 @@
+// LANGUAGE: Kotlin
+// AUTHOR: Yothin Homjan
+// GITHUB: https://github.com/yotheone0909
+
+inline class Name(val s: String) {
+    val length: Int
+        get() = s.length
+
+    fun greet() {
+        println("Hello, $s")
+    }
+}    
+
+fun main() {
+    val name = Name("Kotlin")
+    name.greet() // method `greet` is called as a static method
+    println(name.length) // property getter is called as a static method
+}
