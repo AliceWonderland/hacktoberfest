3dc7625863dd5018d368a5fddf68b29ff7dea6f5
Iago Tito <38673719+iagotito@users.noreply.github.com>
2020-10-03 16:58:39 -0300

    Update iagotito_hello.py

--- scripts/iagotito_hello.py
+++ scripts/iagotito_hello.py
@@ -1,6 +1,6 @@
-// LANGUAGE: Python
-// ENV: iagotito_hello.py
-// AUTHOR: Iago Tito
-// GITHUB: https://github.com/iagotito
+# LANGUAGE: Python
+# ENV: iagotito_hello.py
+# AUTHOR: Iago Tito
+# GITHUB: https://github.com/iagotito
 
 print('Hello, World!')
