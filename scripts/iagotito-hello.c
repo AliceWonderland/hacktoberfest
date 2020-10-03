f9c3018fb75e4f5494809852fed314f61af8d4fe
Iago Tito <38673719+iagotito@users.noreply.github.com>
2020-10-03 16:56:17 -0300

    Create iagotito-hello.c

---
+++ scripts/iagotito-hello.c
@@ -0,0 +1,11 @@
+// LANGUAGE: Javascript
+// ENV: iagotito_hello.js
+// AUTHOR: Iago Tito
+// GITHUB: https://github.com/iagotito
+
+#include <stdio.h>
+
+int main() {
+   printf("Hello, World!");
+   return 0;
+}
