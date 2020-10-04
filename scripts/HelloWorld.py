0093dc7dd5fa7da339b9b7fa4ea3644ef2b8e8e6
Gaurav Gupta <59227470+gauravgupta24@users.noreply.github.com>
2020-10-04 21:35:47 +0530

    Created HelloWorld script in Python

---
+++ scripts/HelloWorld.py
@@ -0,0 +1,7 @@
+#lang: Python
+#bio: Cs Student
+#auth: Gaurav Gupta (https://github.com/gauravgupta24)
+#Github: (https://github.com/gauravgupta24)
+
+print("Hello World!")
+print("Greetings contributors!")
