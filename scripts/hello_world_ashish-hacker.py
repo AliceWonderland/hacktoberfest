a71f001abcbd444e8045b1bf78e52953d27eec5e
Ashish <54330052+ashish-hacker@users.noreply.github.com>
2020-10-05 23:03:45 +0530

    hello world in python

--- scripts/hello_world_ashish-hacker.py
+++ scripts/hello_world_ashish-hacker.py
@@ -1,8 +1,7 @@
 def hello(name='World'):
   return 'Hello {}!'.format(name)
 def main():
-  name = input('Your name?:')
-  helloYou = hello(name)
+  helloYou = hello()
   print(helloYou)
 if __name__ == '__main__':
   main()
