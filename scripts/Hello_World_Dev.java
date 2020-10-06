ff5ce61eea7054d16023c2f44c8f36fdd3fe5190
Dev582 <62136619+Dev582@users.noreply.github.com>
2020-10-06 10:40:11 +0530

    Created Hello_World_Dev.java

---
+++ scripts/Hello_World_Dev.java
@@ -0,0 +1,15 @@
+/**
+  LANGUAGE: JAVA
+  AUTHOR: dev582
+  GITHUB: https://github.com/Dev582
+*/
+
+public class hello_world {
+
+    public static void main(String[] args) {
+    
+        System.out.println("Hello, world!");
+        
+    }
+
+}
