4b590d28699a79cf8489d7b96dd51c0c4fd53fca
Blushsard <72167984+Blushsard@users.noreply.github.com>
2020-10-06 09:03:35 +0200

    Add files via upload

---
+++ scripts/hello_world_Blushsard.c
@@ -0,0 +1,13 @@
+/*
+LANGUAGE : C
+AUTHOR : Blushsard
+Github : https://github.com/AliceWonderland/
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("Bonjour le monde !\n");
+}
