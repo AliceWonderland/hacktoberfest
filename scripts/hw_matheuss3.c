6cc627ff385b2700edcecada2b4d7e4c1890b13d
matheuss3 <matheusspo2000@gmail.com>
2020-10-05 23:25:11 -0300

    add code hw_matheuss3.c on scripts

---
+++ scripts/hw_matheuss3.c
@@ -0,0 +1,13 @@
+/**
+ * Autor: matheuss3 (Matheus Oliveira)
+ * Date: 2020-10-05
+**/
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  printf("matheuss3 :)\n");
+
+  return 0;
+}
