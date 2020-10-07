895c59ca01b58b4579dfc4d9ec56587931e36fb1
Ofeer Levavi <ofeerlev@gmail.com>
2020-10-07 22:10:56 +0300

    my hello world script

---
+++ hello_world_ofeeri.py
@@ -0,0 +1,12 @@
+def anything():
+    print('Hey there, this script can do anything')
+    input('''Seriously type in whatever you want and it'll do it:''')
+
+
+def hello():
+    anything()
+    print('just kidding')
+    print('Hello World')
+
+
+hello()
