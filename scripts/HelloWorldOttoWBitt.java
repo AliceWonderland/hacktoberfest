c626713637929a51a81578b85bc8ea06d4f2d5ac
Abhishek Nallana <60889353+abhinallana@users.noreply.github.com>
2020-10-07 19:51:37 +0530

    hacktober2020

--- scripts/HelloWorldOttoWBitt.java
+++ scripts/HelloWorldOttoWBitt.java
@@ -1,9 +1,9 @@
 // LANGUAGE: Java
-// ENV: HelloWorldOttoWBitt.java
-// AUTHOR: Otto Bittencourt
-// GITHUB: https://github.com/OttoWBitt
-class HelloWorldOttoWBitt{
+// ENV: HelloWorldinjava.java
+// AUTHOR: Abhi Nallana
+// GITHUB: https://github.com/abhinallana
+class HelloWorldinjava{
 	public static void main(String[] args) {
-		System.out.println("Hello Wolrd!!! - HacktoberFest 2017");
+		System.out.println("Hello World !!! Hacktober-2020");
 	}
 }
