8493846c0caab7427683aa0f5a223e0f6993911b
rudranshnath4 <72539668+rudranshnath4@users.noreply.github.com>
2020-10-08 10:57:31 +0530

    Update C++.cpp

--- scripts/C++.cpp
+++ scripts/C++.cpp
@@ -3,7 +3,7 @@ env script
 aut Anshu Musaddi
 github https://github.com/anshucollege*/
 #include<iostream.h>
-int main()
+void main()
 {
-  cout<<" Hello World ";
+  cout<<" Hello! Welcome to my PROGRAM ";
 }
