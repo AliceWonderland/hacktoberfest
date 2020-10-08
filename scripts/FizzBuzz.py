76df8b4cc58c72b5115b3390d8ecb52205d948a5
Hritik Kumar Chaturvedi <58544829+hritikchaturvedi11@users.noreply.github.com>
2020-10-08 18:31:07 +0530

    Hello World and Fizz Buzz code
    This script has both the codes the Hello World Print and FizzBuzz code.

--- scripts/FizzBuzz.py
+++ scripts/FizzBuzz.py
@@ -1,6 +1,10 @@
 # FizzBuzz in python using nested if else
 # Author - @hritikchaturvedi11
 
+# Hello World one line code
+print("Hello World")
+
+#Fizz Buzz Code
 for i in range(1,100):
     if(i % 3 == 0 and i % 5 == 0):
         print('FizzBuzz')
