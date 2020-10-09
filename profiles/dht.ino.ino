88dc28103cad13e85a067ed6e94140168c1a6de1
arpitchandak <arpitchandak123@gmail.com>
2020-10-09 16:59:56 +0530

    Arpit Chandak

---
+++ profiles/dht.ino.ino
@@ -0,0 +1,19 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  delay(1000);
+}
