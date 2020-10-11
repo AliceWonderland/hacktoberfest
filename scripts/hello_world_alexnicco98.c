b85d564468defaaa2e5ed6f37726d86c96531de7
Alessandro Niccolini <45079314+alexnicco98@users.noreply.github.com>
2020-10-11 18:13:32 +0200

    Add files via upload

---
+++ scripts/hello_world_alexnicco98.c
@@ -0,0 +1,10 @@
+// LANGUAGE: C
+// AUTHOR: Alessandro
+// GITHUB: https://github.com/alexnicco98
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hello, World!\n");
+    return 0;
+}
