d702c3307f5f064131c43e02fecadc774731a214
Ashnabasotia <basotiaashna51299@gmail.com>
2020-10-11 05:43:09 +0530

    Hacktoberfest

--- scripts/hello_world_Ashnabasotia.java
+++ scripts/hello_world_Ashnabasotia.java
@@ -1,8 +1,10 @@
 // LANGUAGE: Java
 // AUTHOR: Ashna Basotia
 // GitHub: https://github.com/Ashnabasotia
-
+public class hello_world_Ashnabasotia
+{
 public static void main(String[] args)
 {
     System.out.println("Hello, World!");
+}
 }
