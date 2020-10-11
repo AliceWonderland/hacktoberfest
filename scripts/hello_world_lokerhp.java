62e272fa4eab243c5ba1e96fd460d69fdd486994
lokerhp <44868937+lokerhp@users.noreply.github.com>
2020-10-11 22:25:59 +0200

    Create hello_world_lokerhp.java

---
+++ scripts/hello_world_lokerhp.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// AUTHOR: lokerhp
+// GITHUB: https://github.com/lokerhp
+
+public class hello_world_lokerhp {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
