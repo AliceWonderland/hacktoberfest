3d076702312543a383c3fb854309a4fc7dabeeda
Deepika <deepsjunejadj@gmail.com>
2020-10-12 15:23:44 +0530

    Add HelloWorld script written in Java to scripts directory

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Deepika Juneja
+// GITHUB: https://github.com/deepsjuneja
+
+public class HelloWorld {
+	public static void main(String args[]){
+		System.out.println("Hello World!");
+	}
+}
