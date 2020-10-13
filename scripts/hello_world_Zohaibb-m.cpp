4e1972440dae0061a8d7f011a973dca242b8a7f4
Zohaib Munir <bcsf19m540@pucit.edu.pk>
2020-10-13 16:43:40 +0500

    Hacktober Fest Pull Request

---
+++ scripts/hello_world_Zohaibb-m.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// ENV: Visual Studio Code
+// AUTHOR: Zohaib Munir
+// GITHUB: hhttps://github.com/Zohaibb-m
+
+#include <iostream>
+ using namespace std;
+
+ int main(){
+     cout << "Hello World";
+ }
