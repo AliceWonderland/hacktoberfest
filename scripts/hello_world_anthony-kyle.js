4b61354eabea8fe7089fe25c702c9097aa2816df
Anthony McGrath <akm@anthonykyle.co.nz>
2020-10-15 15:15:33 +1300

    Added Hello World Script

---
+++ scripts/hello_world_anthony-kyle.js
@@ -0,0 +1,13 @@
+/******************************************************************************
+ * LANGUAGE:  JavaScript
+ * ENV:       Node.js
+ * AUTHOR:    Anthony McGrath
+ * GITHUB:    https://github.com/anthony-kyle
+ *****************************************************************************/
+
+const greeting = 'Hello',
+  grammar = ',',
+  recipient = 'World',
+  punctuation = '!'
+
+console.log(`${greeting}${grammar} ${recipient}${punctuation}`)
