0e44246a4dedf948f1fa72984fde8ac6d6efa5bf
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-10-15 23:56:23 -0400

    added script,contributor, and profile files

---
+++ scripts/hello_world_OzLievano.js
@@ -0,0 +1,10 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Osvaldo Lievano
+// GITHUB: https://github.com/OzLievano
+
+const helloWorld = (name) => {
+    console.log(`Hello World, my name is ${name}`)
+}
+
+helloWorld('Ozzy')
