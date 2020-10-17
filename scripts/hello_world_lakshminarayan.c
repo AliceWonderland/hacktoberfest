cebf76fb9cfaa6400d6870e610877f2ced323246
LakshmiNarayanP <54149506+LakshmiNarayanP@users.noreply.github.com>
2020-10-17 20:52:00 +0530

    My first PR - LakshmiNarayan

---
+++ scripts/hello_world_lakshminarayan.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    //Name : Lakshmi Narayan
+    //Language : C
+    //github : https://github.com/LakshmiNarayanP
+    printf("Hello World !");
+}
