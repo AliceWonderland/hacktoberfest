65915e858e6a16a621bc7656b4ba52e82b56b88e
webdesignbydivyansh <62607594+webdesignbydivyansh@users.noreply.github.com>
2020-10-17 13:23:07 +0530

    hellodivyansh.cpp
    Added a new file and printed hello world using C++

---
+++ scripts/hellodivyansh.cpp
@@ -0,0 +1,12 @@
+/*
+  Language: C++
+  Environment: Script
+  Author: Divyansh Kushwaha
+  Github: https://github.com/webdesignbydivyansh
+  */
+  
+  #include<iostream.h>
+  int main()
+  {
+    cout<<"Hello World";
+   }
