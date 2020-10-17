3cf1246906221e5cd0216aae8bd7a3bec25e39ba
webdesignbydivyansh <62607594+webdesignbydivyansh@users.noreply.github.com>
2020-10-17 13:09:41 +0530

    Create hellodiv.cpp

---
+++ scripts/hellodiv.cpp
@@ -0,0 +1,11 @@
+/* Language: C++
+  Environment: Script
+  Author: Divyansh Kushwaha
+  Github: https://github.com/webdesignbydivyansh
+  */
+  
+  #include<iostream.h>
+  int main()
+  {
+    cout<<"Hello World";
+    }
