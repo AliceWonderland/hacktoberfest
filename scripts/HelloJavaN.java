abd611c17d1a98ab1e6fe815d9ab4475648e92b2
Ali Murtaza <58934980+Ali-Murtaza143@users.noreply.github.com>
2020-10-18 19:02:46 +0500

    hacktoberfest
    hacktoberfest

--- scripts/HelloJavaN.java
+++ scripts/HelloJavaN.java
@@ -4,8 +4,10 @@
   GITHUB: https://github.com/Nethmi96
 */
 
-public class HelloJavaN {
+ class HelloJava{
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+       System.out.println("-------hacktoberfest--------");
+        for(int i=0;i<10;i++){
+        System.out.println("the no:",i+1,"Hello world! ");
     }
 }
