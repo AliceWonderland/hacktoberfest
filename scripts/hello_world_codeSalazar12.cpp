0cc0fe742be795ff5eae2d1fa8caf823f28d96c9
codeSalazar12 <codesalazar12@gmail.com>
2020-10-18 10:02:08 +0530

    new file added

---
+++ scripts/hello_world_codeSalazar12.cpp
@@ -0,0 +1,18 @@
+// LANGUAGE: C++
+// ENV: Sublime Text
+// AUTHOR: Priyam Samanta
+// GITHUB: https://github.com/codeSalazar12
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+	cout << "Hello World";
+
+}
