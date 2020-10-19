66eaac6d67f0e4d86ae2a42085dcdc5e099367c9
ARsh-ALgo <72185264+ARsh-ALgo@users.noreply.github.com>
2020-10-19 12:22:56 +0530

    Update C++.cpp
    1. Header file  corrected
    2. Namespace included
    3. return type was not declared

--- scripts/C++.cpp
+++ scripts/C++.cpp
@@ -2,8 +2,11 @@
 env script
 aut Anshu Musaddi
 github https://github.com/anshucollege*/
-#include<iostream.h>
+
+#include<iostream>
+using namespace std;
 int main()
 {
   cout<<" Hello World ";
+  return 0;
 }
