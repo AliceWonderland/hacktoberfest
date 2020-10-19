41dedc7537716e57282e002e3a45c541b140ae86
Sanjana <singhsanjana3104@gmail.com>
2020-10-19 21:25:43 +0530

    Done first commit to learn opensource

---
+++ scripts/hello_world_sanjana.cpp
@@ -0,0 +1,11 @@
+// Language : C++
+//ENV:Not know
+//AUTHOR:Sanjana Singh
+//GITHUB:https://github.com/sanjanasingh31
+
+#include <stdio.h>
+using namespace std;
+int main(){
+    cout<<"Hello world";
+    return 0;
+}
