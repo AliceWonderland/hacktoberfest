19f6e5c048957df2a53de943f583aa98f301c404
Adithya Narayan K A <adithya.narayan2000@gmail.com>
2020-10-20 22:30:52 +0530

    Added helloworld file in CPP

---
+++ hello_world_adithya2000.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: CPP
+// ENV: NA
+// AUTHOR: Adithya Narayan K A 
+// GITHUB: https://github.com/adithya2000
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  cout<<"Hello World!"<<endl;
+  return 0;
+}
