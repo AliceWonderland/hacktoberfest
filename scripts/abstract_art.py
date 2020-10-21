e255e1a90b1651ffec43918bd13088e4199625cb
In Dev <starlightgames.g@gmail.com>
2020-10-21 15:19:38 +0530

    abstract.py

---
+++ scripts/abstract_art.py
@@ -0,0 +1,10 @@
+def abstract():
+    tanmay_turtle.pensize(2)
+    tanmay_turtle.speed(0)
+
+    for i in range(500):
+        tanmay_turtle.shape("circle")
+        tanmay_turtle.forward(i)
+        tanmay_turtle.right(91)
+        
+abstract()
