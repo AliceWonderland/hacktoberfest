ce35fa7a04d2811d78cf3fdfb21ce2a2b7e68692
John Doe <johndoe@example.com>
2020-10-25 18:22:42 +0530

    Added hello_world_tridib2003.cpp file to the scripts

---
+++ scripts/hello_world_tridib2003.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Tridib Samanta
+// GITHUB: https://github.com/tridib2003
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
