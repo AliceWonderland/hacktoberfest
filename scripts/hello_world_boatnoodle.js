252250c5cd753f5b7c245b7ca122065a4c6da6ce
boatnoodle <nattasit31@gmail.com>
2020-10-25 10:07:22 +0700

    ✨ Add hello_word script

---
+++ scripts/hello_world_boatnoodle.js
@@ -0,0 +1,7 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Boatnoodle
+// GITHUB: https://github.com/boatnoodle
+
+const greetingWord = "!!!!!!Hello, World!!!!!!!!";
+console.log(greetingWord);
