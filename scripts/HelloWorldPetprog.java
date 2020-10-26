12712140a07798c6d1ceb78a99058fb66d527c79
Taiwo Farinu <farinupeter@gmail.com>
2020-10-26 11:30:40 +0100

    add author details

--- scripts/HelloWorldPetprog.java
+++ scripts/HelloWorldPetprog.java
@@ -1,3 +1,7 @@
+// LANGUAGE: Java
+// ENV: HelloWorldPetprog
+// AUTHOR: Taiwo Farinu
+// GITHUB: https://github.com/petprog
 public class HelloWorldPetprog {
 
     public static void main(String[] args) {
