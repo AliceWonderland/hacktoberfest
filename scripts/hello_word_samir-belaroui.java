0adcc34714c86c8fa9f050bb7389b94788bb89c3
Samir <41807740+Samir-Belaroui@users.noreply.github.com>
2020-10-28 20:03:35 +0000

    Create hello_word_samir-belaroui.java

---
+++ scripts/hello_word_samir-belaroui.java
@@ -0,0 +1,15 @@
+// LANGUAGE: Java
+// AUTHOR: Samir Belaroui
+// GITHUB: https://github.com/Samir-Belaroui
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	
+	public static void main (String[] args) throws java.lang.Exception{
+		System.out.println("Hello World");
+	}
+	
+}
