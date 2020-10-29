6cb55e92b352731a2c62959fc2b69a3955d30d68
amruthajm <43128144+amruthajm@users.noreply.github.com>
2020-10-29 17:47:55 +0530

    Create hello_world_amruthajm.cpp

---
+++ scripts/hello_world_amruthajm.cpp
@@ -0,0 +1,16 @@
+// LANGUAGE: C++
+// AUTHOR: Amrutha J M
+// GITHUB: https://github.com/amruthajm
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+  cout<<"Hello, World!";
+  
+  return 0;
+
+}
