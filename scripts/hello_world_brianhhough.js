efdba7b9cebc2304498831a3693dace153501499
BrianHHough <brianhoughmedia@gmail.com>
2020-10-29 09:05:55 -0400

    Created "Hello World" script
    Used JavaScript to create a "Hello World" for Hacktoberfest 🎃

---
+++ scripts/hello_world_brianhhough.js
@@ -0,0 +1,6 @@
+// LANGUAGE: JavaScript
+// ENV: Node.js
+// AUTHOR: @BrianHHough
+// GITHUB: https://github.com/brianhhough
+
+console.log("Hello World, it's your friendly, neighborhood programmer.") 
