0a8b57aeeb59981e0a4a6552c23bb1da82b9bda0
Bhavin Sanchaniya <sanchaniyabhavin8499@gmail.com>
2020-10-31 20:02:46 +0530

    Hello World

---
+++ scripts/hello_world_bhavin.java
@@ -0,0 +1,20 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Bhavin Sanchaniya
+  GITHUB: https://github.com/bhavin8499
+*/
+
+import java.util.Scanner;
+
+public class HelloWorldNewVibe {
+    public static void main(String[] args) {
+        System.out.println("Please Enter Your name so we can give you a suprise");
+        
+        Scanner in = new Scanner(System.in);
+        String user = in.nextLine();
+                
+                
+        System.out.println("Hello Programmer!"+ user + "\n Are you doing all right");
+        System.out.println("Welcome to Programming!");
+    }
+}
