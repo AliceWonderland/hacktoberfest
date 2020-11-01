7033f936eedcfbe0fe9262f21c825a23a92d89a2
Raymond Chen <Sizbei@users.noreply.github.com>
2020-10-31 23:34:47 -0400

    Create HelloWorld1.cs

---
+++ scripts/HelloWorld1.cs
@@ -0,0 +1,20 @@
+/** 
+* LANGUAGE: C#
+* AUTHOR: Raymond Chen
+* GITHUB: https://github.com/sizbei
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine();
+        }
+    }
+}
