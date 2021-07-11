9bb3a34218a509a2639a03b32dc18e64d1633707
Ayushi Gupta <gayushi790@gmail.com>
2021-07-12 00:40:53 +0530

    Created Hello script.php

---
+++ scripts/hello_world_ayushi790.php
@@ -0,0 +1,11 @@
+<?php
+// LANGUAGE: PHP
+// ENV: PHP
+// AUTHOR: Ayushi Gupta
+// GITHUB: https://github.com/ayushi790/
+
+echo "Hello World!";
+echo "Hi there!";
+echo "This is my contribution...";
+
+?>
