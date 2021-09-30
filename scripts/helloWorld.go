f00af6533ceb287582aedecb7634eea2d305b21e
Apoorv <tofu.develops@gmail.com>
2021-09-30 22:25:52 +0530

    added http hello world

--- scripts/helloWorld.go
+++ scripts/helloWorld.go
@@ -1,1 +1,17 @@
+//apoorvcodes 
 
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", HelloWorld)
+    http.ListenAndServe(":8080", nil)
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World, %s!", r.URL.Path[1:])
+}
