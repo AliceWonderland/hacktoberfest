092145fe0e0823adfcc726179d5225c4d4dbd797
huzaifa <hutariq@nisum.com>
2021-10-01 15:31:04 +0500

    Hacktoberfest first commit

---
+++ scripts/hello_world_huzaifa50.js
@@ -0,0 +1,10 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Huzaifa Tariq
+// GITHUB: https://github.com/huzaifa50
+
+const printHelloWorld = () => {
+  return "Hello World";
+};
+
+console.log(printHelloWorld());
