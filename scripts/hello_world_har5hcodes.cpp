731c9b5fcfa77224380bdf37d4afd7408ac5f068
harshwandr <harshavardhanputta5@gmail.com>
2021-10-01 08:50:47 +0530

    Commit message

---
+++ scripts/hello_world_har5hcodes.cpp
@@ -0,0 +1,18 @@
+// LANGUAGE: C++
+// AUTHOR: Harshavardhan Putta
+// GITHUB: https://github.com/har5hcodes
+
+#include <iostream>
+
+using namespace std;
+
+void sayHelloWorld()
+{
+    cout << "Hello, World!" << endl;
+}
+
+int main()
+{
+    sayHelloWorld();
+    return 0;
+}
