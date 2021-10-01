d6a63b763e9c7da19f4608ec21ecadde97ef1067
farihaNaqvi <71366962+farihaNaqvi@users.noreply.github.com>
2021-10-01 16:59:31 +0530

    Add files via upload

---
+++ scripts/Hello_Everyone_farihaNaqvi.py
@@ -0,0 +1,6 @@
+#Lang Python3.8
+#env Interactive as well as script
+#aut Fariha Naqvi
+#github https://github.com/farihaNaqvi
+print("Hello Everyone!")
+print("Fariha is here to Say Hello Everyone!")
