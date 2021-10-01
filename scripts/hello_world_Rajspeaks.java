662dd08e2222502a31da6c3f387ad9e11888a807
Rajdeep Das <44817007+Rajspeaks@users.noreply.github.com>
2021-10-02 02:47:58 +0530

    Create hello_world_Rajspeaks.java

---
+++ scripts/hello_world_Rajspeaks.java
@@ -0,0 +1,6 @@
+//Java Profram to print hello woeld    
+class hello_world_Rajspeaks{  
+        public static void main(String args[]){  
+         System.out.println("Hello World");  
+        }  
+    }  
