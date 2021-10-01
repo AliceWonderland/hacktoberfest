b27e250050fe35404ff946532d632d4e8dd936b9
SHOURYA GUPTA <86829917+ShouryaBrahmastra@users.noreply.github.com>
2021-10-02 00:00:46 +0530

    Create Hello_World.c
    By Shourya Gupta

---
+++ scripts/Hello_World.c
@@ -0,0 +1,12 @@
+// LANGUAGE: C
+// ENV: C.c
+// AUTHOR: Shourya Gupta
+// GITHUB: https://github.com/ShouryaBrahmastra
+
+#include<stdio.h>
+
+int main(void)
+{
+  printf("Hello World!");
+  return (0);
+}
