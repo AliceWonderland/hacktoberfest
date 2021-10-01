3a39986c43f068134ccde117e67c517c78e0917a
rajanyadav857 <rajanyadav857@gmail.com>
2021-10-02 02:36:10 +0530

    adding hello_world_rajan.java file

---
+++ scripts/hello_world_rajan.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Rajan Kumar Yadav
+// GITHUB: https://github.com/rajanyadav857
+
+class HelloWorldByRajan {
+    public static void main(String ar[]) {
+        System.out.println("Hello World");
+    }
+}
