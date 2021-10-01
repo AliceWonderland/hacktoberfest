6696311dece060abf591f57312eb8a2af27347ec
KrishJani <krishjani01@gmail.com>
2021-10-01 17:38:32 +0530

    Contributed :)

---
+++ scripts/Hello worlddddd.java
@@ -0,0 +1,11 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Krish Jani
+  GITHUB: https://github.com/KrishJani
+*/
+
+public class HelloWorlddddd {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
