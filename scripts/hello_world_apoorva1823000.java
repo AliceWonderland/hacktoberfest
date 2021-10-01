25a6cb77c3ddccd2df01f26623b64f13740d7823
Apoorva <apoorva.me1@gmail.com>
2021-10-01 07:33:21 +0530

    hello_world_apoorva1823000 added

---
+++ scripts/hello_world_apoorva1823000.java
@@ -0,0 +1,9 @@
+//LANGUAGE: Java
+// AUTHOR: Apoorva
+// GITHUB: https://github.com/apoorva1823000
+
+public class Hello_world_hacktoberfest{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
