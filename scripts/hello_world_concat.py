02c01267bd878db4e17a5b46a0d518ca0a1bfb85
SASIN83 <48416870+SASIN83@users.noreply.github.com>
2021-10-02 18:48:38 +0530

    Hello world using concatenation

---
+++ scripts/hello_world_concat.py
@@ -0,0 +1,8 @@
+# LANGUAGE: Python
+# ENV: Python3
+# AUTHOR: Sasin Nisar
+# GITHUB: https://github.com/SASIN83
+
+print('Hello, World')
+print('Hello,'+' World')
+print(' '.join(['Hello,','World']))
