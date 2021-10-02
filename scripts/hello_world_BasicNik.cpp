7bbe8cdc7fecc394289d39e37527c67f0e525094
Nikhil Verma <basicnik1798@gmail.com>
2021-10-02 23:40:04 +0530

    Added New Hello World!

---
+++ scripts/hello_world_BasicNik.cpp
@@ -0,0 +1,17 @@
+/*# AUTHOR: Nikhil Verma.
+# LANGUAGE: C++
+# GITHUB: https://github.com/BasicNik
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    char a[30]={'H','e','l','l','o',' ','W','o','r','l','d','!','\0'};
+
+    cout<<a;
+    
+    return 0;
+}
