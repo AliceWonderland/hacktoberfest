459c32695e5b0acb8d33ef4679465bc9a6ee88dd
Shweta <2002shwetarani@gmail.com>
2021-10-02 14:04:53 +0530

    Added a Hello World in JavaScript

---
+++ scripts/helloworld_by_shweta.js
@@ -0,0 +1,9 @@
+// LANGUAGE: JavaScript
+// ENV: Node.js
+// AUTHOR: Shweta Rani
+// GITHUB: https://github.com/Shweta-Rani1
+
+const sayHello = name =>
+  console.log(`Hello ${name}!`);
+
+sayHello('World');
