f6379ebe38f4daa48df46880308e4ac73905dd5e
Krishnkant <krishnkantpachori50@gmail.com>
2021-10-02 20:58:08 +0530

    Add hello_world_Dcode-k.js.

---
+++ scripts/hello_world_Dcode-k.js
@@ -0,0 +1,14 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: AKrishnkant  Pachori
+// GITHUB: https://github.com/Dcode-k
+
+function helloWorld(message){
+	if(message === "Hello World"){
+		console.log(message);
+	} else {
+		console.log("This entered the wrong message!");
+	}
+}
+
+helloWorld("Hello World");
