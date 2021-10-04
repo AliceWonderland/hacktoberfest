a77dfbd37a7d36bbbd7ff58abed2f5e32cf372af
Aditya singh <singh.aditya2726@gmail.com>
2021-10-04 19:13:58 +0530

    HelloWorld.java

---
+++ scripts/HelloJava.java
@@ -0,0 +1,11 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Aditya Singh
+  GITHUB: https://github.com/Aditya2603
+*/
+
+public class HelloJavaN {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
