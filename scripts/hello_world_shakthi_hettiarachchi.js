b97c77484e511378db213b47e603e92397ac9ccf
Shakthi Dilshan <shakthi.dilshan5@gmail.com>
2021-10-04 06:50:57 +0530

    added Hello World Script to script dir

---
+++ scripts/hello_world_shakthi_hettiarachchi.js
@@ -0,0 +1,8 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Shakthi Hettiarachchi
+// GITHUB: https://github.com/shakthidilshan
+
+var hello = "Hello World !";
+
+console.log("|------------------------------------------|\n |               "+hello+"              |\n |------------------------------------------|");
