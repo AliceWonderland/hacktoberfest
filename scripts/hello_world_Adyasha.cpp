ee86e92f3285c152092ac5dbeaf323182b8696fb
Adyasha Samal <adyasamal2001@gmail.com>
2021-10-06 02:22:34 +0530

    requiredd files added

---
+++ scripts/hello_world_Adyasha.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// ENV: GCC
+// AUTHOR: Adyasha Samal
+// GITHUB: https://github.com/Adyasha28
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Hello World"<<endl;
+}
