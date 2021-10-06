287a8f7844252f5f3604231908600c83e7224c69
Yashvardhan Baid <codechaser2002@gmail.com>
2021-10-06 12:16:34 +0530

    Create codechaser.cpp

---
+++ scripts/codechaser.cpp
@@ -0,0 +1,18 @@
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--||author : codechaser||-- 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"Hello World!\n";
+    return 0;
+}
+/*
+    |---------------------------------------------------|
+    |||   https://codeforces.com/profile/codechaser   |||
+    |||   https://www.codechef.com/users/codechaser   |||
+    |||        https://github.com/code-chaser         |||
+    |---------------------------------------------------|
+*/
