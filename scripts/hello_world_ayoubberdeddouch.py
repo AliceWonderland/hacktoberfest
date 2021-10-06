79554d3cac87c0ca166e7a9eb5b35b63286941e0
Ayoub BERDEDDOUCH <ayoub.berd@gmail.com>
2021-10-06 17:58:33 +0200

    Create hello_world_ayoubberdeddouch.py

---
+++ scripts/hello_world_ayoubberdeddouch.py
@@ -0,0 +1,8 @@
+// LANGUAGE: Python
+// ENV: Python / Anaconda
+// Version: 3.XX
+// Donwload from: https://www.python.org/ | https://www.anaconda.com/products/individual
+// AUTHOR: Ayoub Berdeddouch
+// GITHUB: https://github.com/ayoub-berdeddouch
+
+print("Hello, world")
