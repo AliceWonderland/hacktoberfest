497c7fc666c068627aa5a914ca9efb4cde490899
Luziane Freitas <freitas.lu@outlook.com>
2021-10-07 09:55:07 -0300

    challenge 3

---
+++ scripts/hello_word_luzianeFreitas.js
@@ -0,0 +1,6 @@
+// LANGUAGE: JavaScript
+// ENV: Node.js
+// AUTHOR: Luziane Freitas
+// GITHUB: https://github.com/LuzianeFreitas
+
+console.log("Hello, world! I'm Luzi and I'm enthusiastic front end!");
