56ede10f082b2e6c3a2d00e9a52fed48ebc0348a
githubANURAG2 <anurag0429rai@gmail.com>
2021-10-09 23:07:19 +0530

    Added my profile

---
+++ scripts/hello-world-ANURAG.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ int n;
+    cout<<"enter no of times to print hello world :";
+    cin>>n;
+    cout<<endl;
+    while(n--)
+	    cout << "Hello World..!" << endl;
+	return 0;
+}
