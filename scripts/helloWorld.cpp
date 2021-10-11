506816dbf0d6a255a3e363bf584dcf52bd0e3781
Anshika Sharma <anshikasharma0556@gmail.com>
2021-10-11 20:21:06 +0530

    'helloWorld.cpp'

---
+++ scripts/helloWorld.cpp
@@ -0,0 +1,14 @@
+//Language: C++                       ......Any line starting with // is a comment in C++
+//Author: Anshika Sharma
+//Github: https://github.com/Anshika321
+
+#include <iostream>                 //The #include is a preprocessor directive used to include files in our program. By using this, we are including the contents of the iostream file.
+
+using namespace std;                //This statement is used to import the entirety of the std namespace into the current namespace of the program. When we import a namespace we are essentially pulling all type definitions into the current scope.
+
+int main(){                        //The main() function is the driver function. The execution of code beings from this function.
+
+    cout << "Hello world! \n" ;         //cout prints the content inside the quotation marks.
+    return 0;                               //; is used to indicate the end of a statement.
+
+}                                   //The curly braces indicate the start and the end of the function.
