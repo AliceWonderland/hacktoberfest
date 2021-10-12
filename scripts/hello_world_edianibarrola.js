7e88c9948e5a7196923dda15a20712607081930a
edianibarrola <edianibarrola@gmail.com>
2021-10-12 13:52:02 +0000

    commiting 3 files for edian ibarrola

---
+++ scripts/hello_world_edianibarrola.js
@@ -0,0 +1,9 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Edian Ibarrola
+// GITHUB: https://github.com/edianibarrola
+let isEdiCool = null;
+if (isEdiCool == null){
+    isEdiCool = "Yes, of course my horse!"
+    console.log('Hello, World!');
+}
