e5810017a3981278ccdd4ce24e4775b2a3f25f9c
Naresh Chandanbatve <chandanbatven@gmail.com>
2021-10-12 12:32:48 +0530

    Create Hello_World_Naresh_chandanbatve.java

---
+++ scripts/Hello_World_Naresh_chandanbatve.java
@@ -0,0 +1,11 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Naresh Chandanbatve
+  GITHUB: https://github.com/Naresh-chandanbatve
+*/
+
+public class Hello_World_Naresh_chandanbatve {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
