8e23699fc1387dece83bb0f7dc1e2b2a0fb85743
Pratiksha Patnaik <prpatnai@redhat.com>
2021-10-12 12:53:07 -0400

    Made my first commit to hacktoberfest

---
+++ scripts/hello_world_pratster95.py
@@ -0,0 +1,7 @@
+# LANGUAGE: Python
+# AUTHOR: Pratiksha Patnaik
+# GITHUB: https://github.com/Pratster95/
+
+from datetime import date
+todays_date = date.today()
+print("Hello World !\n Let's do some hacking from", todays_date)
