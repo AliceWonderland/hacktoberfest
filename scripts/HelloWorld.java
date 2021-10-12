eca8c768f339e000635102231cf60ed110b02219
SwargaRajDutta <72154312+Swarga-codes@users.noreply.github.com>
2021-10-13 00:30:16 +0530

    Created script in JAVA.
    Successfully created and coded a program in JAVA that prints "Hello World!" as the output.

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Swarga Raj Dutta
+  GITHUB: https://github.com/Swarga-codes
+*/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
