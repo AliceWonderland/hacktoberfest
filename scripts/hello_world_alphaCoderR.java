833ed2a0ea3d4db7a57172613fd7892b257161bd
alphaCoderR <eldrago2000@gmail.com>
2021-10-14 00:00:14 +0530

    hacktoberfest_pr

---
+++ scripts/hello_world_alphaCoderR.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// ENV: Java
+// AUTHOR: Swaroop Kumar Sahu
+// GITHUB: https://github.com/alphaCoderR
+
+class hello_world_alphaCoderR{  
+    public static void main(String args[]){  
+     System.out.println("Hello, World!");  
+    }  
+}
