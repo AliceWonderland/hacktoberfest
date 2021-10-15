c018d8cefb37c18d0bfd590f39609586bd07d81d
Luana <1295943@sga.pucmias.br>
2021-10-15 19:00:32 -0300

    hello Luana

---
+++ scripts/hello_world_lucatake.java
@@ -0,0 +1,12 @@
+// LANGUAGE: Java
+// AUTHOR: Luana Takeishi
+// GITHUB: https://github.com/Lucatake
+
+public class hello_world_luacatake {
+    public static void say_hello(){
+        System.out.println("Hello, world!");
+    }
+    public static void main(String[] args) {
+        say_hello();
+    }
+}
