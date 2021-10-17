fae9bc00a5a30a7fc0b813f2e49b9371bcdefefe
Suruchi <78213044+jhaSuruchi@users.noreply.github.com>
2021-10-17 19:00:46 +0530

    Add a script to the scripts directory

---
+++ scripts/hello_world_suruchi.java
@@ -0,0 +1,16 @@
+/*
+ * LANGUAGE: JAVA
+ * AUTHOR: Suruchi
+ * GITHUB: https://github.com/jhaSuruchi
+ * 
+ * **/
+
+public class HelloWorldPadrones {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}
+
+}
