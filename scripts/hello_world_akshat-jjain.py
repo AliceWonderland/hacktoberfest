9e2c8f06707acbb9ce710d40e305d0db9a317314
AKSHAT JAIN <akshat9983@gmail.com>
2021-10-20 11:51:54 +0530

    Update and rename helloworld_akshat-jjain.py to hello_world_akshat-jjain.py

---
+++ scripts/hello_world_akshat-jjain.py
@@ -0,0 +1,6 @@
+// LANGUAGE: Python
+// ENV: Python
+// AUTHOR: Akshat Jain
+// GITHUB: https://github.com/akshat-jjain
+    
+print("Hello World By Akshat Jain")
--- scripts/helloworld_akshat-jjain.py
+++
@@ -1,1 +0,0 @@
-print("Hello World By Akshat Jain")
