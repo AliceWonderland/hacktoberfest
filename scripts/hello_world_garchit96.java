a1a5bd568268fda7286ed5f6ca0a3fa2c2df326d
garchit96 <74394417+garchit96@users.noreply.github.com>
2021-10-21 12:02:12 +0530

    Create hello_world_garchit96.java

---
+++ scripts/hello_world_garchit96.java
@@ -0,0 +1,11 @@
+//Language : Java
+//Author : Archit Gupta
+//GitHUb Profile: https://github.com/garchit96
+
+public class Hacktoberfest{
+	public static void main(String[] args){
+			System.out.println("Hello, World!");
+		}
+}
+
+
