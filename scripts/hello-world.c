e72f8551b89eaa931dd3bccf8301cdd4d9701bd5
Supuni Saradha <supuni.saradha@xitricon.com>
2021-10-24 15:40:22 +0530

    ADD hello world

---
+++ scripts/hello-world.c
@@ -0,0 +1,11 @@
+-- LANGUAGE: C
+-- 
+-- AUTHOR:supuni96
+-- GITHUB: https://github.com/supuni96
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
