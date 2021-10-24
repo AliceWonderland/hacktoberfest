132666266fa1d34da017ebfc9de23bba6c0a6cd2
Rathod Ritesh <Rathodriteshrock@gmail.com>
2021-10-24 16:44:57 +0530

    add script of hello world in  pascal

---
+++ scripts/hello_world.pas
@@ -0,0 +1,13 @@
+{
+    LANGUAGE: Pascal
+    ENV: Free Pascal
+    AUTHOR: Rathod Ritesh
+    GITHUB: https://github.com/Ri2rathod
+}
+program HelloWorld;
+uses crt;
+
+begin
+   writeln('Hello, World!');
+   readkey;
+end. 
