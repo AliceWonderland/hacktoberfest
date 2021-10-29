f528c34608cf5839f78b83b71c16da2cd02859fe
Alexandre Matheus <alexandrefenix1@hotmail.com>
2021-10-29 13:29:29 -0300

    Update HelloWorld.cs
    hacktoberfest

--- scripts/HelloWorld.cs
+++ scripts/HelloWorld.cs
@@ -13,7 +13,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Hello world!");
             Console.ReadLine();
         }
     }
