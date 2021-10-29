a3e39751f38a3b0c0a64503961f13da6af4a80de
K A R T H I K <karthik.lal558@gmail.com>
2021-10-29 20:53:34 +0530

    hacktoberfest: Created (hello_World in C++)
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

---
+++ scripts/hello_world_karthik558.cpp
@@ -0,0 +1,16 @@
+/* Lang C++
+env script
+autor K A R T H I K
+github https://github.com/karthik558 */
+#include<iostream.h>
+#include<conio.h>
+
+// using namespace std ();
+
+int main()
+{
+  clrscr ();
+  cout<<" Hello World ";
+  getch();
+  return 0;
+}
