fddc8f6264131307eb393588c5cb4678811bf542
Vaibhavpan02 <89473295+Vaibhavpan02@users.noreply.github.com>
2021-10-30 23:36:09 +0530

    Create hello_world_Vaibhavpan02.c

---
+++ scripts/hello_world_Vaibhavpan02.c
@@ -0,0 +1,15 @@
+// LANGUAGE:C
+// ENV:C.c
+// AUTHOR: Alice Chuang
+// GITHUB: https://github.com/Vaibhavpan02
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  clrscr();
+  printf("Hello World!);
+  printf(Welcome to the World!);
+         return 0;
+}
+         
