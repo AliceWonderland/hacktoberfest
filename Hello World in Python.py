d41f869806473ac7585a8f149fca577724228d8a
Daksheshapkare <113005270+Daksheshapkare@users.noreply.github.com>
2022-09-18 23:47:13 +0530

    Create  Hello World in Python.py

---
+++ Hello World in Python.py
@@ -0,0 +1,11 @@
+#Lang Python2
+
+ #env Interactive as well as script 
+
+ #aut Dakshesh Apkate
+
+ #  https://github.com/Daksheshapkare
+
+ print "Hello World" 
+
+ print "Dakshesh was here to Say Hello World!"
