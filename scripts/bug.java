76fa450cee4339226c3f707ee1b3253f2c90ef86
Prince Kumar Raj <66669293+Princekumaraj@users.noreply.github.com>
2022-10-01 02:14:54 +0530

    Create bug.java

---
+++ scripts/bug.java
@@ -0,0 +1,11 @@
+public class Demo //class definition  
+{  
+public static void main(String args[])  
+{  
+void display()  
+{  
+System.out.println("Welcome to Hacktoberfest");  
+}  
+//statements  
+}  
+}  
