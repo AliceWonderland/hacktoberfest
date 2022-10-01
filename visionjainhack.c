63ad0268a3ad8ac2fbe2528cad6996d41e98ca26
Vision Jain <71940170+visionjain@users.noreply.github.com>
2022-10-01 15:34:19 +0530

    visionjainhack.c

---
+++ visionjainhack.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  printf("heelo world hacktoberfest");
+  printf("heelo world hacktoberfest");
+  printf("heelo world hacktoberfest");
+  printf("heelo world hacktoberfest");
+  printf("heelo world hacktoberfest");
+  
+    
+    
+    
+  return 0;
+  
+  }
+     
