fdc1bb91e077639f036779f61da1f39eb23e545a
Nayaker <nayak.primary@gmail.com>
2022-10-01 17:27:02 +0530

    first open source contribution

---
+++ scripts/Nayaker.cpp
@@ -0,0 +1,43 @@
+// Language : C++ 
+// Code Snippet from my codeforces 
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <iomanip>
+#include <iterator>
+#include <set>
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define fr(i,n) for (ll i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std; 
+
+const int MOD = 1e9 + 7; 
+
+void solve(){
+    cout<<"Hello World, I am Atanu Nayak a 19 year old coder."<<endl;
+}
+    
+int main(){
+    fast
+    
+    #ifndef ONLINE_JUDGE
+    freopen( "input.txt" , "r" , stdin);
+    #endif
+    
+    // ull tc; cin>>tc;
+    ull tc = 1;
+    while (tc--)
+    solve();
+    
+    return 0;
+}
