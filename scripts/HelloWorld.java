809076e005ff79841ffbfebd2b4586e42892db42
Rupesh Dubey <91791257+rupeshexe@users.noreply.github.com>
2022-10-01 14:28:34 +0530

    Create HelloWorld.java

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Rupesh Dubey
+// GITHUB: https://github.com/rupeshexe
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
