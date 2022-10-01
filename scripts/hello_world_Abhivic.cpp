900bb8a06cecb9f4fb09c3d3cd373897b6170d24
Abhivic <110732765+Abhivic000@users.noreply.github.com>
2022-10-01 21:36:59 +0530

    Commit message

---
+++ scripts/hello_world_Abhivic.cpp
@@ -0,0 +1,12 @@
+
+//* LANGUAGE: C++
+//* AUTHOR: Abhishek Singh
+//* GITHUB: https://github.com/Abhivic000
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello, World!";
+    return 0;
+}
