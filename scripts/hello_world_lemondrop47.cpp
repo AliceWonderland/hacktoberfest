9862a07da1bb4bd48ff3fa4144852e3659f4ebfd
LemonDrop847 <66952539+LemonDrop847@users.noreply.github.com>
2022-10-02 07:52:39 +0530

    Commit message

---
+++ scripts/hello_world_lemondrop47.cpp
@@ -0,0 +1,25 @@
+// LANGUAGE: C++
+// ENV: GCC
+// AUTHOR: LemonDrop847
+// GITHUB: https://github.com/lemondrop847
+
+#include <bits/stdc++.h>
+using namespace std;
+// defs
+#define ll long long int
+#define nl "\n"
+#define rep(i, n) for (ll i = 0; i < n; i++)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    rep(i,1)
+        cout<<"He";
+    rep(i,2)
+        cout<<"l";
+    rep(i,1)
+        cout<<"o World";
+    return 0;
+}
