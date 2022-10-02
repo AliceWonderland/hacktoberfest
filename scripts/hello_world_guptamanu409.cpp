130b1d6731c47aae6203085bbce6d9b06508c2a3
Tanjiro <guptamanu409@gmail.com>
2022-10-02 13:30:09 +0530

    First Contribution

---
+++ scripts/hello_world_guptamanu409.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// ENV: VS Code
+// AUTHOR: Vishal Gupta
+// GITHUB: https://github.com/guptamanu409
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  cout<<" Hello World ";
+  return 0;
+}
