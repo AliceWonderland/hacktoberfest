587428360f4f1d4670e95c155824bf1134764682
Anshuman Shukla <Anshumanshukla46@gmail.com>
2022-10-03 17:15:01 +0530

    Added Hello World

---
+++ scripts/hello_world_Anshuman.java
@@ -0,0 +1,8 @@
+public class hello_world_Anshuman{
+    public static void main(String []args){
+        
+        System.out.println("Hello World!");
+        System.out.println("Hacktoberfest 2022");
+        
+    }
+}
