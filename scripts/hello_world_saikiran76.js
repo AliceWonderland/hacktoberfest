5e071365c3f691a0bac69beb3bf9bccfd20791b4
unknown <kskkorada76@gmail.com>
2022-10-05 00:13:40 +0530

    commited

---
+++ scripts/hello_world_saikiran76.js
@@ -0,0 +1,14 @@
+// LANGUAGE : JAVASCRIPT
+// ENV : Node.js
+// AUTHOR : saikiran76
+// GITHUB : https://github.com/saikiran76
+
+// Here i'm gonna create a hello world script using
+// javascipt functions
+
+var world = function(name){
+    console.log("Hello, World"+" "+"by"+" "+name);
+}
+
+var my_name = "kiran";
+world(my_name);
