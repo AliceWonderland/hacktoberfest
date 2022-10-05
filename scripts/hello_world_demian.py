1caa87ba7de683e4c0a5beb578f9812cd2e8044b
Demian143 <patrickxavier140@gmail.com>
2022-10-04 22:39:33 -0300

    adding my python script

---
+++ scripts/hello_world_demian.py
@@ -0,0 +1,14 @@
+# LANGUAGE: Python
+# ENV: python3
+# AUTHOR: Demian143
+# GITHUB: https://github.com/Demian143
+
+""" Just a func to print your name """
+
+def print_name(name: str) -> str:
+    print(f"Hello: {name}!")
+
+
+if __name__ == "__main__":
+    print_name("World")
+
