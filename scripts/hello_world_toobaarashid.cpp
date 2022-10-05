c4a4d97303b3bb064c19594a76e648fce082e920
msameerfarooq <me.sameer.farooq@gmail.com>
2022-10-05 20:50:05 +0500

    Added Hello World in C++

---
+++ scripts/hello_world_toobaarashid.cpp
@@ -0,0 +1,12 @@
+
+// LANGUAGE: C++
+// ENV: Visual Studio
+// AUTHOR: Tooba Rashid
+// GITHUB: https://github.com/toobaarashid
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World"<<endl;
+}
