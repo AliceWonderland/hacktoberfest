c8b80ba79a770014ebd04c7d5c26911172a7c217
Leo-paul <leopaulmartin@hotmail.fr>
2022-10-05 16:32:13 +0200

    add leopaul29 contribution

---
+++ scripts/hello_world_leopaul29.java
@@ -0,0 +1,11 @@
+//Language : Java
+//Author : Léo-paul MARTIN
+//GitHUb Profile: https://github.com/leopaul29
+
+
+public class Leopaul29 {
+
+	public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
