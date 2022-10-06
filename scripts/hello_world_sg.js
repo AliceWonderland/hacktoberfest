92ff92e6abc2d3d7b9154700015af5e223112cdf
Amy <amy.pyae@smbe.fujielectric.com>
2022-10-06 13:30:26 +0800

    Added Profile

---
+++ scripts/hello_world_sg.js
@@ -0,0 +1,10 @@
+// LANGUAGE: JavaScript
+// ENV: Node.js
+// AUTHOR: Amy 
+// GITHUB: https://github.com/amydev-me
+'use strict'
+
+const hello = "Hello World"
+
+console.log(hello);
+alert(hello);
