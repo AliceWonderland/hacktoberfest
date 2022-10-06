84458906cf438bf830ab68876bc5f29b4c5ebc2c
Manmohan Singh Sajwan <108542436+rishi-kai21@users.noreply.github.com>
2022-10-06 19:03:09 +0530

    Created hello$world
    committed a change

---
+++ scripts/hello$world.java
@@ -0,0 +1,12 @@
+// LANGUAGE: Java
+// ENV: Node.js
+// AUTHOR: Manmohan Singh Sajwan
+// GITHUB: https://github.com/rishi-kai21
+
+class hello$world{
+  
+  public static void main(String args[]){
+    
+    System.out.println("Hello World");
+  }
+}
