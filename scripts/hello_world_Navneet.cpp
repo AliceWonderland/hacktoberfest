71d747f09efe53851818912469f372da593f6474
Navneet Gupta <navneetguptaend@gmail.com>
2022-10-06 23:06:26 +0530

    Update Details

---
+++ scripts/hello_world_Navneet.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Navneet Gupta
+// GITHUB: https://github.com/Nav21000
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
