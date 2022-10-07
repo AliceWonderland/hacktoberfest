82e09181c3bd8de0e5fe9f20616c9fe10fd146bd
Kajal <kajalkumari36240@gmail.com>
2022-10-07 23:58:38 +0530

    Commit by Kajal Kumari

---
+++ scripts/hello_world_kajalkumari23.java
@@ -0,0 +1,11 @@
+// LANGUAGE: Java
+// ENV: java
+// AUTHOR: Kajal Kumari
+// GITHUB: https://github.com/kajalkumari23
+
+import java.util.*;
+class Test {   
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+};
