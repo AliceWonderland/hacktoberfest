ed766cfda0398fc61335ee2816fe579c1552a432
Nandini Singh <Ns.Nandinising@gmail.com>
2022-10-08 20:04:10 +0530

    hacktoberfest2022

---
+++ Nandini_singh.md
@@ -0,0 +1,17 @@
+# Nandini Singh
+
+### Location
+
+Mahaashtra, India
+
+### Academics
+
+Chhattisgarh Swami vikekananda Technical University
+
+### Interests
+
+- Reading, painting and Gardening
+
+### Profile Link
+
+[Nandini Singh](https://shuu05.github.io/Portfolio/)
---
+++ hello_world_NANDINI_SINGH.py
@@ -0,0 +1,7 @@
+# LANGUAGE: Python
+# AUTHOR:   NANDINI SINGH
+# GITHUB:   https://github.com/shuu05
+
+print("Hello, World!");
+# The print function is used here to print whatever has been placed inside the brackets.
+# The inverted commas tell the compiler that the given item is a string.
