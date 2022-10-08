0946bbf98290c3b7c80cdf6d701f597416c26862
Anika Kamath <87904385+anika-kamath@users.noreply.github.com>
2022-10-08 21:56:49 +0530

    Added Java code

---
+++ scripts/hello_world_anika.java
@@ -0,0 +1,13 @@
+// LANGUAGE: Java
+// ENV: JVM
+// AUTHOR: Anika K
+// GITHUB: https://github.com/anika-kamath
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        
+    }
+}
