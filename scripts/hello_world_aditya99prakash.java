638c1a4b432d4f0f24a296ded0f48b146ab88d24
aditya99prakash <36444408+aditya99prakash@users.noreply.github.com>
2022-10-08 13:57:26 +0530

    Create hello_world_aditya99prakash.java

---
+++ scripts/hello_world_aditya99prakash.java
@@ -0,0 +1,13 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Aditya Prakash
+  GITHUB: https://github.com/aditya99prakash
+*/
+
+public class Chathumina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
