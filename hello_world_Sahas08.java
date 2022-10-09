8d7a4258f25627ab2219c2b8d156ddd99751744c
Sahas <43925194+Sahas08@users.noreply.github.com>
2022-10-09 17:53:09 +0530

    Create hello_world_Sahas08.java
    Made a file greeting our fellow coders!

---
+++ hello_world_Sahas08.java
@@ -0,0 +1,11 @@
+// LANGUAGE: Java
+// AUTHOR: Sahas Saxena
+// GITHUB: https://github.com/Sahas08
+
+public class hello_world_Sahas08
+{
+  public static void main(String args[])
+  {
+    System.out.println("Hello, World!");
+  }
+}
