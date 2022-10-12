4e3f3ae05dfbb6681940ce2c1040019e3286ffe8
shreyash2610 <57799693+shreyash2610@users.noreply.github.com>
2022-10-12 11:58:47 +0530

    Create Hello_Shreyash.js

---
+++ scripts/Hello_Shreyash.js
@@ -0,0 +1,6 @@
+// LANGUAGE: JavaScript
+// ENV: Node.js
+// AUTHOR: Shreyash Gupta
+// GITHUB: https://github.com/shreyash2610
+
+console.log(“Hello World! I’m Shreyash Gupta, a data scientist and also worked in amazon.”);
