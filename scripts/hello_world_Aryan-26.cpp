c4bf7e9245a959bcc8e8df8da27b7b489e415c92
Aryan <97938438+Aryan-26@users.noreply.github.com>
2022-10-12 09:44:11 +0530

    hello_world_Aryan-26.cpp

---
+++ scripts/hello_world_Aryan-26.cpp
@@ -0,0 +1,10 @@
+// LANGUAGE: C++
+// ENV: hello.cpp
+// AUTHOR: Aryan Panchal
+// GITHUB: https://github.com/Aryan-26
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  cout<<"Hello, World!";
+  return 0;
+}
