a328ff940b8330ca707367bc6dc5e16c6e44a364
dvir019 <30556126+dvir019@users.noreply.github.com>
2022-10-13 22:38:31 +0300

    Create insertiobSort.py

---
+++ scripts/insertiobSort.py
@@ -0,0 +1,9 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
