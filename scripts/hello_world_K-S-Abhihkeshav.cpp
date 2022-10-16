ecaf247ee956d18aacbf3c5ec0239a1a11cb71bb
AbhihKurella <ks21csb0f03@student.nitw.ac.in>
2022-10-16 13:44:38 +0530

    added and made changes to hello world file

--- scripts/hello_world_K-S-Abhihkeshav.cpp
+++ scripts/hello_world_K-S-Abhihkeshav.cpp
@@ -1,6 +1,9 @@
+// LANGUAGE: C++
+// AUTHOR: Abhihkeshav
+// GITHUB: https://github.com/K-S-Abhihkeshav
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"hello world";
+	cout<<"Hello, World!";
 	return 0;
 }
