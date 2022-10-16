1552f9058b2af44f91780565923552462dce48f0
Kumar Ashish Ranjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2022-10-16 23:57:56 +0530

    adding the codes by Ashish

---
+++ scripts/ashish.js
@@ -0,0 +1,6 @@
+// LANGUAGE: JS
+// AUTHOR: Kumar Ashish Ranjan
+// GITHUB: https://github.com/dev-AshishRanjan
+console.log("====================================");
+console.log("Hello World!");
+console.log("====================================");
---
+++ scripts/ashish.jsx
@@ -0,0 +1,13 @@
+// LANGUAGE: REACTJS
+// AUTHOR: Kumar Ashish Ranjan
+// GITHUB: https://github.com/dev-AshishRanjan
+
+import React from 'react'
+
+const Ashish = () => {
+  return (
+    <div>Hello world!</div>
+  )
+}
+
+export default Ashish;
