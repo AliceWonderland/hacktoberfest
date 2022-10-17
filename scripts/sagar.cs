5e81ccd1d4aef3b66840c6fe43d1e68312e77251
SAAGAR <38255285+Sagar-Timalsena@users.noreply.github.com>
2022-10-17 13:56:37 +0545

    Create sagar.cs

---
+++ scripts/sagar.cs
@@ -0,0 +1,20 @@
+/** 
+* LANGUAGE: C#
+* AUTHOR: Sagar Timalsena
+* GITHUB: https://github.com/Sagar-Timalsena**/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World Nepal Is Heaven!");
+            Console.ReadLine();
+        }
+    }
+}
