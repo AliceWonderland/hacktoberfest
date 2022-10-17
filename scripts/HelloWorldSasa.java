1aeaf44b53654da1f13430a9b8472e16158266ca
Sasanka <sajewsasanka1@gmail.com>
2022-10-17 11:27:02 +0530

    Create HelloWorldSasa.java

---
+++ scripts/HelloWorldSasa.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// ENV: HelloWorldSasa.java
+// AUTHOR: Sasanka
+// GITHUB: https://github.com/sasankaweera123
+
+class HelloWorldSasa{
+	public static void main(String[] args) {
+		System.out.println("Hello Wolrd! - HacktoberFest 2022");
+	}
+}
