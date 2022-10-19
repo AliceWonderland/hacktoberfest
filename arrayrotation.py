2b6163c22657327c98a7bcda20278c27c29c192e
Rolins Macwan <macwanrolins4567@gmail.com>
2022-10-19 13:36:18 +0530

    Create arrayrotation.py

---
+++ arrayrotation.py
@@ -0,0 +1,23 @@
+
+# function to rotate array by d elements using temp array
+def rotateArray(arr, n, d):
+    temp = []
+    i = 0
+    while (i < d):
+        temp.append(arr[i])
+        i = i + 1
+    i = 0
+    while (d < n):
+        arr[i] = arr[d]
+        i = i + 1
+        d = d + 1
+    arr[:] = arr[: i] + temp
+    return arr
+ 
+ 
+# Driver function to test above function
+arr = [1, 2, 3, 4, 5, 6, 7]
+print("Array after left rotation is: ", end=' ')
+print(rotateArray(arr, len(arr), 2))
+ 
+# this code is contributed by Anabhra Tyagi
