0d8cb7a0a85506880801238d2f5459413566a8e1
Prashant Kumar <pkumars397@gmail.com>
2022-10-20 12:16:32 +0530

    Create Square.cpp

---
+++ Square.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n=5;
+  for(int row=1;row<=n;row++){
+    for(int col=1;col<=n;col++{
+      cout<<"* ";
+    }
+      cout<<endl;
+   }
+ }
