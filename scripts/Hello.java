c0db7abe88f817376125c6e2c4149472ba923486
Bodhdipta Roy <79841195+bodhdipta-roy@users.noreply.github.com>
2022-10-21 19:53:40 +0530

    Create Hello.java

---
+++ scripts/Hello.java
@@ -0,0 +1,11 @@
+// LANGUAGE: Java
+// AUTHOR: Bodhdipta Roy
+// GITTHUB: https://github.com/bodhdipta-roy
+
+class Hello
+{
+    public static void main(String[] args)
+    {
+	 System.out.println("Hello World!");
+    }
+}
