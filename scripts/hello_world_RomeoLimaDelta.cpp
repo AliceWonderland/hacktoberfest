551a990d9961aa537ba25297db01e5f93f6cb924
RPLD2001 <26rajpreet@gmail.com>
2022-10-21 13:27:57 +0530

    Added files and updated CONTRIBUTORS.md

---
+++ scripts/hello_world_RomeoLimaDelta.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Rajpreet Lal Das
+// GITHUB: https://github.com/RomeoLimaDelta
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  cout<<"Hello, World";
+  return 0;
+}
