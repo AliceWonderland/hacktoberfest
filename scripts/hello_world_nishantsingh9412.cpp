ec7212ad142dfa0a659d905398a22e2bdd6f8b0c
Nishant Singh <59526404+Nishantsingh9412@users.noreply.github.com>
2022-10-24 00:15:40 +0530

    Create hello_world_nishantsingh9412.cpp

---
+++ scripts/hello_world_nishantsingh9412.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: c++
+// ENV: VS code 
+// AUTHOR: Nishant Singh
+// GITHUB: https://github.com/nishantsingh9412
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World";
+	return 0;
+}
