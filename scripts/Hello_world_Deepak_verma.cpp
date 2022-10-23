6361a0e9d864f35f8cefe7c723f1d7b6f60914bc
Deepak Verma <deepak.verma.met20@itbhu.ac.in>
2022-10-23 11:41:39 +0530

    New Changes
    Added all 3 things

---
+++ scripts/Hello_world_Deepak_verma.cpp
@@ -0,0 +1,12 @@
+// LANGUAGE: C++
+// ENV: VS Code
+// AUTHOR: Deepak Verma
+// GITHUB: https://github.com/Deepakv14
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World1" << endl;
+}
