52a37cf6d2befaf0667f8b3017487d9fd87bb9f8
Shahbaz Alam <88643897+Alam9835@users.noreply.github.com>
2022-10-25 14:05:52 +0530

    Create hello_world_Alam9835.java

---
+++ scripts/hello_world_Alam9835.java
@@ -0,0 +1,10 @@
+//Language : Java
+//Author : Shahbaz Alam
+//GitHUb Profile: https://github.com/Alam9835
+
+
+public classHacktoberfest2022{
+	public static void main(String[] args){
+			System.out.println("Hello, World!");
+		}
+}
