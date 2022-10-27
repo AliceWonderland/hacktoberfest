dabfa283a44bb75c8fddb325d9148eb0a509224b
Avani <55042694+avanisaxena9@users.noreply.github.com>
2022-10-27 19:16:57 +0530

    created hello_world_avanisaxena9.java file
    this is a contribution as  a part of hacktoberfest

---
+++ scripts/hello_world_avanisaxena9.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Avani Saxena
+// GITHUB: https://github.com/avanisaxena9
+
+class helloWorld {
+public static void main(String[] args) {
+System.out.println("Hello World!");
+}
+}
