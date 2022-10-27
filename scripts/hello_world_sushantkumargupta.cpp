1f41c8daafb54b66753c0da10dd174fe14fe2fa8
SUSHANT KUMAR GUPTA <92776685+sushantkumargupta@users.noreply.github.com>
2022-10-27 13:06:17 +0530

    Create hello_world_sushantkumargupta.cpp

---
+++ scripts/hello_world_sushantkumargupta.cpp
@@ -0,0 +1,10 @@
+// LANGUAGE: C++
+// AUTHOR: sushantkumargupta
+// GITHUB: https://github.com/sushantkumargupta
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	cout<<"Hello World";
+	return 0;
+}
+// Happy Coding :-)
