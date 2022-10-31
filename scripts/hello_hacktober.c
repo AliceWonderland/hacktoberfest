cd62b9669876cc7d21bfcd8ec601572840d36cf9
Subhashish Mahapatra <92688857+SubhashishMahapatra@users.noreply.github.com>
2022-10-31 22:45:07 +0530

    Added Hello-world script in c-Language
    For Hacktober Fest.

---
+++ scripts/hello_hacktober.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+   // printf(): inside paramenter youu can write anything which you wanted to print under inverted comma.
+   printf("Hello, Hacktober Fest!");
+   return 0;
+}
