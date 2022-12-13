03705339dbfb815bd0fced12cae82e668c180026
ChikangaTakudzwa <takudzwachikanga@outlook.com>
2022-12-13 23:14:31 +0000

    Added name to contributions and script

---
+++ scripts/hello_world_chikanga.py
@@ -0,0 +1,8 @@
+# LANGUAGE: Python
+# ENV: Linux
+# AUTHOR: Chikanga Takudzwa
+# GITHUB: https://github.com/ChikangaTakudzwa
+
+myName = "Chikanga Takudzwa"
+
+print("Hello, world script by: ", myName)
