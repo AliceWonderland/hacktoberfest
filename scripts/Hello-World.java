11d2d4fa8325088ea72062e078fe401a41868ea1
BHARATH GADDAM <134089638+bharathgaddam1712@users.noreply.github.com>
2023-09-04 17:03:38 +0530

    Create Hello-World.java
    hello-world in java

---
+++ scripts/Hello-World.java
@@ -0,0 +1,12 @@
+/*
+  LANGUAGE: PYTHON
+  AUTHOR: BHARATH GADDAM
+  GITHUB: https://github.com/bharathgaddam1712
+*/
+
+Public class Bharath {
+    public static void main(strings[] args) {
+       system.out.println("hello world");
+     }
+}
+        
