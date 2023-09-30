a71f3928e4d538f013e3c53496a4d1a92fb9237f
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2023-10-01 01:19:06 +0530

    Tast 3 completed | create script page

---
+++ scripts/hello_world_sharma62.js
@@ -0,0 +1,6 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: SURAJ KUMAR SHARMA
+// GITHUB: https://github.com/sharma62
+
+console.log('Hello, Suraj Welcome to Hacktoberfest event to keep enjoy & contribute & win.');
