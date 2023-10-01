4d7e98799b90906cbe298405a5c0e20f051a8603
milindaShehan <milinda.msd@gmail.com>
2023-10-01 20:42:44 +0530

    added scripts/hello_world_milindaShehan.java

---
+++ scripts/hello_world_milindaShehan.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// AUTHOR: Milinda Shehan
+// GITHUB: https://github.com/milindaShehan
+
+public class hello_world_milindaShehan {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
