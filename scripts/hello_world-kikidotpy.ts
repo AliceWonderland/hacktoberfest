069654c77a362bace42e0f0b99f2677de531a66c
Chiara <81535309+KikiDotPy@users.noreply.github.com>
2023-10-01 15:13:09 +0200

    Create hello_world-kikidotpy.ts
    Add hello world script in ts

---
+++ scripts/hello_world-kikidotpy.ts
@@ -0,0 +1,8 @@
+// LANGUAGE: Typescript
+// ENV:      Node.js
+// AUTHOR:   Chiara Di Sano
+// GITHUB:   https://github.com/KikiDotPy
+const salutation : String = 'Hello'
+const enviroment : String = 'World'
+
+console.log(`${salutation}, ${enviroment}!`)
