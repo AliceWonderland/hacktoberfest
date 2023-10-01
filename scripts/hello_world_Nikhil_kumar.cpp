0059821cf8ae7a360b0995da6011a842dc82003b
Nikhil Kumar <108209923+Nikhil743@users.noreply.github.com>
2023-10-01 12:45:40 +0530

    Updated

---
+++ scripts/hello_world_Nikhil_kumar.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Nikhil kumar
+// GITHUB: https://github.com/Nikhil743
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello World!\n";
+	return 0;
+}
