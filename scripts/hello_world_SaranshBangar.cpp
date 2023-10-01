6c71070f8c9521e41ff96c9b2557e0aae9c2dc43
SaranshBangar <saranshbangad@gmail.com>
2023-10-01 05:30:57 +0530

    Added myself

---
+++ scripts/hello_world_SaranshBangar.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// ENV: VS Code
+// AUTHOR: Saransh Bangar
+// GITHUB: https://github.com/SaranshBangar
+
+#include <iostream>
+using namespace std;
+void main()
+{
+    cout<<"Hello World!";
+}
