a57a533945031f1f47507b503e1dde918adeb4d5
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-10-07 13:11:37 +0530

    Create hello_world_Rohan-San.cpp

---
+++ scripts/hello_world_Rohan-San.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Rohan Santhosh
+// GITHUB: https://github.com/Rohan-San
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
