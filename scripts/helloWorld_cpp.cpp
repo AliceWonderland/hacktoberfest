2325db103ebdf5602905b387200fe636b2572bc3
mokshanirgutti <mokshasai910@gmail.com>
2023-10-08 17:05:26 +0530

    Added helloworld code in cpp

---
+++ scripts/helloWorld_cpp.cpp
@@ -0,0 +1,12 @@
+// LANGUAGE: C++
+// AUTHOR: Moksha
+// GITHUB: https://github.com/mokshanirugutti
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!";
+    return 0;
+}
