a326120b7d981578e6b31c54fcf95e84a5b35078
Prashareet <prashareet@gmail.com>
2023-10-09 00:29:21 +0530

    added correct github link

--- scripts/hello_world_prashareet.js
+++ scripts/hello_world_prashareet.js
@@ -1,6 +1,6 @@
 // LANGUAGE: Javascript
 // ENV: Node.js
 // AUTHOR: Prashareet Choudhury
-// GITHUB: https://github.com/AliceWonderland
+// GITHUB: https://github.com/prashareet
 
 console.log('Hello, World!');
