b73979dba77b3627211b953cd1d8abe25390e70d
Rakshit Benjwal <rakshitbenjwal8@gmail.com>
2023-10-10 19:28:03 +0530

    Create HelloWorld.cpp

---
+++ scripts/HelloWorld.cpp
@@ -0,0 +1,10 @@
+// LANGUAGE: C++
+// AUTHOR: RakshitBenjwal
+// GITHUB: https://github.com/Rakshit358
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"Hello World"<<endl;
+  return 0;
+}
