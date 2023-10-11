1a51dba46100536d220bb26f11f3f35ca4be4563
urveesh2812 <urveeshm123@gmail.com>
2023-10-11 23:35:47 +0530

    changed files directories

---
+++ scripts/HelloWorld_Urveesh.js
@@ -0,0 +1,11 @@
+//LANGUAGE : Javascript
+//ENV Node.js
+//Author Urveesh Mathur
+//Github : https://github.com/AliceWonderland
+
+
+let greeting=()=>{
+	console.log("Hello World!");
+}
+
+greeting();
