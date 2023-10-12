525f9f0a49f35119619460e3a36efef85e5f2cad
VishnuSandeep1108 <78595337+VishnuSandeep1108@users.noreply.github.com>
2023-10-12 14:19:56 +0530

    update Scripts

---
+++ scripts/hello_world_VishnuSandeep1108.cpp
@@ -0,0 +1,12 @@
+// LANGUAGE: C++
+// ENV: GCC
+// AUTHOR: Vishnu Sandeep
+// GITHUB: https://github.com/VishnuSandeep1108/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Hello World"<<endl;
+    return 0;
+}
