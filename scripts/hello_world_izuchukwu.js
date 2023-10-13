6bf426faf6e3aff57ceb56403c5d833f02ffbc66
izuchukwu obi <izuchukwuobi997@gmail.com>
2023-10-13 20:35:58 +0100

    Carried out all three tasks successfully

--- scripts/hello_world_izuchukwu.js
+++
@@ -1,5 +0,0 @@
-//Language: JavaScript
-//Author: Izuchukwu Obi
-
-const greeting = str => str
-greeting('Hello World')
---
+++ scripts/hello_world_izustic.js
@@ -0,0 +1,8 @@
+//LANGUAGE: JavaScript
+// ENV: Node.js
+//AUTHOR: Izuchukwu Obi
+// GITHUB: https://github.com/izustic
+
+
+const greeting = str => str
+greeting('Hello World!')
