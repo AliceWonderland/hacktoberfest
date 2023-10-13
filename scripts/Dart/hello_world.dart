92e9645e697557bab25d27152980ed2b3c1f437c
= <=>
2023-10-13 15:13:25 +0700

    Add:Dart folder & add:hello_world_MuizzuddinRifqi.dart

---
+++ scripts/Dart/hello_world.dart
@@ -0,0 +1,12 @@
+// LANGUAGE: Dart
+// ENV: Flutter
+// AUTHOR: Muizzuddin Rifqi
+// GITHUB: https://github.com/MuizzuddinRifqi
+
+void main() {
+  String greeting = "Hello,";
+  String target = "World";
+  String coolModifier = "Awesome";
+  
+  print('$greeting $target $coolModifier!');
+}
