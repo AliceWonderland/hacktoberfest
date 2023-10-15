b3b288ab31851ab589a58a7718de1ffb559c6898
AbhishekPandey2203 <114644422+AbhishekPandey2203@users.noreply.github.com>
2023-10-15 10:36:51 +0530

    Create If-else_AbhishekPandey2203.cpp

---
+++ scripts/If-else_AbhishekPandey2203.cpp
@@ -0,0 +1,19 @@
+// LANGUAGE: C++
+// ENV: Node.js
+// AUTHOR: AbhishekPandey2203
+// GITHUB: https:// AbhishekPandey2203
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+  int a=10;
+  if(a>10)
+  {
+    cout<<"Hello"<<endl;
+  }
+
+  else
+    cout<<"World"<<endl;
+}
