8ef064d7383b437082a28394ae9d3e0c7a22c6c5
Prabhutva Kakkar <115348972+PRABHUTVA-KAKKAR@users.noreply.github.com>
2023-10-17 23:31:40 +0530

    Create hello_world_PRABHUTVA-KAKKAR.java
    3RD STEP DONE-CREATED HELLO WORLD PROGRAM IN JAVA

---
+++ scripts/hello_world_PRABHUTVA-KAKKAR.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// ENV: jdk 
+// AUTHOR: PRABHUTVA KAKKAR
+// GITHUB: https://github.com/PRABHUTVA-KAKKAR
+public class hello_world_PRABHUTVA-KAKKAR {
+  public static void main(String[] args){
+    System.out.println("Hello World");
+  }
+}
