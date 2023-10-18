99fa90243be477187dfb49a3f6dbfef5ed0fa02a
Gede Satyamahinsa <satyamahinsaa.pu@gmail.com>
2023-10-18 21:28:54 +0700

    edit CONTRIBUTORS.md and add hello_world_satyamahinsa.c

---
+++ scripts/hello_world_satyamahinsa.c
@@ -0,0 +1,13 @@
+// LANGUAGE: C
+// ENV: gcc
+// AUTHOR: satyamahinsa
+// GITHUB: https://github.com/satyamahinsa/
+
+// This is a simple program Hello World using C language
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+  return 0;
+}
