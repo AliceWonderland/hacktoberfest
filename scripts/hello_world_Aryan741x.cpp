e3e13bdfd59e3e4515b8a3fb51b2814c6c6acae7
Aryan Sharma <128162387+Aryan741x@users.noreply.github.com>
2023-10-24 15:56:11 +0530

    Create hello_world_Aryan741x.cpp

---
+++ scripts/hello_world_Aryan741x.cpp
@@ -0,0 +1,13 @@
+//LANGUAGE: C++
+//ENV: Compiler .cpp
+//AUTHOR: Aryan741x
+//GITHUB: https://github.com/Aryan741x
+
+//CODE BELOW
+
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"Hello World"<<endl;
+  return 0;
+}
