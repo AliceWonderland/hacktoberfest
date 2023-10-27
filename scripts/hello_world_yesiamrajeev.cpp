5f25f529c812578bbb16d6c75d887a79a2cbe964
rajee <kumarrajeevbehera2019@gmail.com>
2023-10-27 20:43:17 +0530

    Hello Rajeev

---
+++ scripts/hello_world_yesiamrajeev.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// ENV: GCC
+// AUTHOR: Rajeev Kumar Behera
+// GITHUB: https://github.com/yesiamrajeev
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Hello Hack Octo Fest"<<endl;
+}
