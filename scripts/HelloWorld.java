108cfe7ee9941fe37b723dec2f568fda851adf06
Vinayak Sharma <80938731+vinayak19-alt@users.noreply.github.com>
2023-10-29 16:27:31 +0530

    Create HelloWorld.java

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Vinayak Sharma
+  GITHUB: https://github.com/vinayak19-alt
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
