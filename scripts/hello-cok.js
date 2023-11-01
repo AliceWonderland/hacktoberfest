e1a637b1861a0ac9d736107fc3869a681c78e762
Fatkul Iqbal Rosyadi <afat.karno3@gmail.com>
2023-11-02 01:50:52 +0700

    Commit message

--- scripts/hello-cok.js
+++ scripts/hello-cok.js
@@ -1,2 +1,6 @@
-// the hello world program
-console.log('Hello World');
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: fatkul iqbal
+// GITHUB: https://github.com/fatkuliqbal
+
+console.log('Hello, World!');
