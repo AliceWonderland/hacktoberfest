a158c0be9284abb705d6f3656d568d47c69ebbca
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2023-11-06 16:15:57 -0600

    Contribución de zutjmx

---
+++ scripts/Zutjmx.java
@@ -0,0 +1,14 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Jesús Zúñiga Trejo
+  GITHUB: https://github.com/zutjmx
+*/
+
+public class Zutjmx {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, Mundo, desde México!");
+        System.out.println("Autor: zutjmx@gmail");
+    }
+
+}
